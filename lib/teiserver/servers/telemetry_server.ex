bac3ab2e2190a0dc093c4c658680bb63e58778c0
Teifion <teifion@teifion.co.uk>
2021-05-10 20:29:07 +0100

    Improved telemetry server structure slightly

--- lib/teiserver/servers/telemetry_server.ex
+++ lib/teiserver/servers/telemetry_server.ex
@@ -3,16 +3,42 @@ defmodule Teiserver.TelemetryServer do
   alias Teiserver.Client
   alias Teiserver.Battle
 
-  @tick_period 5000
+  @tick_period 5_000
+  @db_cycles 60_000 / @tick_period
 
+  @impl true
   def handle_info(:tick, state) do
-    clients = Client.list_client_ids() |> Enum.count
-    battles = Battle.list_battle_ids() |> Enum.count
+    state = get_telemetry(state)
+    report_telemtry(state)
 
-    :telemetry.execute([:teiserver, :client], %{count: clients}, %{})
-    :telemetry.execute([:teiserver, :battle], %{count: battles}, %{})
+    new_cycle = if state.cycle == 0 do
+      db_store_telemetry(state)
+      @db_cycles
+    else
+      state.cycle - 1
+    end
 
-    {:noreply, state}
+    {:noreply, %{state | cycle: new_cycle}}
+  end
+
+  @spec db_store_telemetry(Map.t()) :: :ok
+  defp db_store_telemetry(_state) do
+    # Store telemtry in the database here
+    :ok
+  end
+
+  @spec get_telemetry(Map.t()) :: Map.t()
+  defp get_telemetry(state) do
+    %{state |
+      clients: Client.list_client_ids() |> Enum.count,
+      battles: Battle.list_battle_ids() |> Enum.count,
+    }
+  end
+
+  @spec report_telemtry(Map.t()) :: :ok
+  defp report_telemtry(state) do
+    :telemetry.execute([:teiserver, :client], %{count: state.clients}, %{})
+    :telemetry.execute([:teiserver, :battle], %{count: state.battles}, %{})
   end
 
   # Startup
@@ -20,9 +46,14 @@ defmodule Teiserver.TelemetryServer do
     GenServer.start_link(__MODULE__, opts[:data], opts)
   end
 
+  @impl true
   def init(_opts) do
     :timer.send_interval(@tick_period, self(), :tick)
 
-    {:ok, %{}}
+    {:ok, %{
+      clients: 0,
+      battles: 0,
+      cycle: @db_cycles
+    }}
   end
 end
