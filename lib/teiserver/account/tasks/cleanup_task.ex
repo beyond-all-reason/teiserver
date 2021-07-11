88e85075da3671c3503bc53a84864fdb142f9ad7
Teifion <teifion@teifion.co.uk>
2021-07-11 11:01:01 +0100

    Minor bug fixes

--- lib/teiserver/account/tasks/cleanup_task.ex
+++ lib/teiserver/account/tasks/cleanup_task.ex
@@ -8,6 +8,8 @@ defmodule Teiserver.Account.Tasks.CleanupTask do
   @spec perform(any) :: :ok
   def perform(_) do
     # Find all users who are muted or banned
+    # we have these anti-nil things to handle if the job
+    # runs just after startup the users may not be in the cache
     Account.list_users(
       search: [
         mute_or_ban: true
@@ -18,12 +20,14 @@ defmodule Teiserver.Account.Tasks.CleanupTask do
       User.get_user_by_id(userid)
       |> check_muted()
       |> check_banned()
+      |> Enum.filter(fn u -> u != nil end)
       |> User.update_user(persist: true)
     end)
 
     :ok
   end
 
+  defp check_muted(nil), do: nil
   defp check_muted(user) do
     if User.is_muted?(user) do
       user
@@ -32,6 +36,7 @@ defmodule Teiserver.Account.Tasks.CleanupTask do
     end
   end
 
+  defp check_banned(nil), do: nil
   defp check_banned(user) do
     if User.is_banned?(user) do
       user
