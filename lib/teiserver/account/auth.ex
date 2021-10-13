d1a761993599cd046a7899cae19e092866571146
Teifion <teifion@teifion.co.uk>
2021-10-14 00:11:36 +0100

    Added full_chat page

--- lib/teiserver/account/auth.ex
+++ lib/teiserver/account/auth.ex
@@ -12,6 +12,7 @@ defmodule Teiserver.Account.Auth do
   def authorize(:smurf_search, conn, _), do: allow?(conn, "teiserver.moderator.account")
   def authorize(:banhash_form, conn, _), do: allow?(conn, "teiserver.moderator.account")
   def authorize(:edit, conn, _), do: allow?(conn, "teiserver.moderator.account")
+  def authorize(:full_chat, conn, _), do: allow?(conn, "teiserver.moderator")
   def authorize(:update, conn, _), do: allow?(conn, "teiserver.moderator.account")
   def authorize(_, conn, _), do: allow?(conn, "teiserver.admin.account")
 end
