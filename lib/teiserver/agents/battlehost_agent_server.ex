b379897fa44a8edc1a3063890f715d18e8c4ccf3
Teifion <teifion@teifion.co.uk>
2022-01-07 15:49:53 +0000

    Updated settings

--- lib/teiserver/agents/battlehost_agent_server.ex
+++ lib/teiserver/agents/battlehost_agent_server.ex
@@ -11,8 +11,8 @@ defmodule Teiserver.Agents.BattlehostAgentServer do
 
   @map_hash "1565299817"
   @game_hash "393009621"
-  @game_name "Beyond All Reason test-18456-3542052"
-  @engine_version "105.1.1-714-ge909643 BAR105"
+  @game_name "Beyond All Reason test-18609-5136d88"
+  @engine_version "105.1.1-739-ga241485 BAR105"
 
   def handle_info(:startup, state) do
     socket = AgentLib.get_socket()
