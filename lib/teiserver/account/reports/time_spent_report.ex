22671af664c988ce625e287951d8e7c70b1aae72
Teifion <teifion@teifion.co.uk>
2022-04-21 13:25:24 +0100

    Time spent report refactor

--- lib/teiserver/account/reports/time_spent_report.ex
+++ lib/teiserver/account/reports/time_spent_report.ex
@@ -1,6 +1,8 @@
 defmodule Teiserver.Account.TimeSpentReport do
   alias Central.Helpers.DatePresets
-  alias Teiserver.Account
+  alias Teiserver.{Telemetry}
+  alias Central.Helpers.TimexHelper
+  import Central.Helpers.StringHelper, only: [get_hash_id: 1]
 
   @spec icon() :: String.t()
   def icon(), do: "fa-regular fa-watch"
@@ -9,35 +11,7 @@ defmodule Teiserver.Account.TimeSpentReport do
   def run(_conn, params) do
     params = apply_defaults(params)
 
-    # Date range
-    {start_date, end_date} =
-      DatePresets.parse(
-        params["date_preset"],
-        params["start_date"],
-        params["end_date"]
-      )
-
-    start_date = (Timex.to_unix(start_date) / 60) |> round
-    end_date = (Timex.to_unix(end_date) / 60) |> round
-
-    data = Account.list_users(
-      search: [
-        data_greater_than: {"last_login", start_date |> to_string},
-        data_less_than: {"last_login", end_date |> to_string},
-        data_equal: {"bot", "false"}
-      ],
-      joins: [:user_stat],
-      order_by: {:data, "rank", :desc},
-      limit: 100
-    )
-    |> Enum.sort(fn (user1, user2) ->
-      v1 = user1.user_stat.data[params["mode"]]
-      v2 = user2.user_stat.data[params["mode"]]
-      v1 >= v2
-    end)
-    |> Enum.filter(fn user ->
-      user.user_stat.data["total_minutes"] != nil
-    end)
+    data = get_data(params)
 
     assigns = %{
       params: params,
@@ -47,12 +21,65 @@ defmodule Teiserver.Account.TimeSpentReport do
     {data, assigns}
   end
 
+  defp get_data(%{"account_user" => "---"}) do
+    []
+  end
+
+  defp get_data(params) do
+    # Date range
+    {start_date, end_date} =
+      DatePresets.parse(
+        params["date_preset"],
+        params["start_date"],
+        params["end_date"]
+      )
+
+    userid = get_hash_id(params["account_user"]) |> to_string
+
+    columns = Telemetry.list_server_day_logs(
+      search: [
+        start_date: start_date,
+        end_date: end_date
+      ],
+      order: "Oldest first",
+      limit: :infinity
+    )
+      |> Enum.map(fn log ->
+        %{
+          "key" => log.date,
+          "Total" => Map.get(log.data["minutes_per_user"]["total"], userid, 0),
+          "Player" => Map.get(log.data["minutes_per_user"]["player"], userid, 0),
+          "Spectator" => Map.get(log.data["minutes_per_user"]["spectator"], userid, 0),
+          "Lobby" => Map.get(log.data["minutes_per_user"]["lobby"], userid, 0),
+          "Menu" => Map.get(log.data["minutes_per_user"]["menu"], userid, 0),
+        }
+      end)
+
+    lines = ~w(Total Player Spectator Lobby Menu)
+      |> Enum.map(fn name -> [name | build_line(columns, name)] end)
+
+    keys = columns
+      |> Enum.map(fn %{"key" => key} -> key |> TimexHelper.date_to_str(format: :ymd) end)
+
+    %{
+      lines: lines,
+      keys: keys
+    }
+  end
+
+  @spec build_line(list, String.t()) :: list()
+  defp build_line(logs, field_name) do
+    logs
+      |> Enum.map(fn log -> log[field_name] end)
+      # |> Enum.map(mapper_function)
+  end
+
   defp apply_defaults(params) do
     Map.merge(%{
       "date_preset" => "This month",
       "start_date" => "",
       "end_date" => "",
-      "mode" => "player_minutes"
+      "account_user" => "#3, Teifion"
     }, Map.get(params, "report", %{}))
   end
 end
