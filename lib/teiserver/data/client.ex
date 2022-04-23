56914767ee4278945aad29b904137b2321efb51f
Teifion <teifion@teifion.co.uk>
2022-04-23 17:17:23 +0100

    Removed disconnect logs

--- lib/teiserver/data/client.ex
+++ lib/teiserver/data/client.ex
@@ -305,8 +305,6 @@ defmodule Teiserver.Client do
 
   # If it's a test user, don't worry about actually disconnecting it
   defp do_disconnect(client, reason) do
-    is_test_user = String.contains?(client.name, "test_user_") or String.contains?(client.name, "InAndOutAgentServer_")
-
     Lobby.remove_user_from_any_battle(client.userid)
     Room.remove_user_from_any_room(client.userid)
     leave_rooms(client.userid)
@@ -315,19 +313,6 @@ defmodule Teiserver.Client do
       Telemetry.increment(:bots_disconnected)
     else
       Telemetry.increment(:users_disconnected)
-    end
-
-    # If it's a test user then it can mean the db connection is closed
-    # we don't actually care about logging out most of them and the
-    # ones we do won't be called test_user_
-    if not is_test_user do
-      if client.name != nil and String.contains?(client.name, "test_user") == false do
-        if reason do
-          Logger.error("disconnect of #{client.name} (#{reason})")
-        else
-          Logger.error("disconnect of #{client.name}")
-        end
-      end
     end
 
     # Typically we would only send the username but it is possible they just changed their username
