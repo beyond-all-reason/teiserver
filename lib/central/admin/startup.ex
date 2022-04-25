4e64cdb5b7fe89f4bd4886e429f36199c3f37205
Teifion <teifion@teifion.co.uk>
2022-04-26 00:01:41 +0100

    Fixed permissions form

--- lib/central/admin/startup.ex
+++ lib/central/admin/startup.ex
@@ -3,12 +3,6 @@ defmodule Central.Admin.Startup do
   use CentralWeb, :startup
 
   def startup do
-    add_permission_set("debug", "debug", ~w(debug))
-    add_permission_set("admin", "user", ~w(show create update delete report))
-    add_permission_set("admin", "dev", ~w(developer structure))
-    add_permission_set("admin", "group", ~w(show create update delete report))
-    add_permission_set("admin", "admin", ~w(limited full))
-
     add_site_config_type(%{
       key: "user.Enable one time links",
       section: "User permissions",
