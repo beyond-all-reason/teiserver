8511532ee50fb8e78dbf229d6b93c40fe0c2f5c3
Teifion <teifion@teifion.co.uk>
2022-07-12 13:26:59 +0100

    Typespecs added

--- lib/teiserver_web/views/admin/user_view.ex
+++ lib/teiserver_web/views/admin/user_view.ex
@@ -1,6 +1,9 @@
 defmodule TeiserverWeb.Admin.UserView do
   use TeiserverWeb, :view
 
+  @spec view_colour :: atom()
   def view_colour(), do: Teiserver.Account.UserLib.colours()
+
+  @spec icon :: String.t()
   def icon(), do: Teiserver.Account.UserLib.icon()
 end
