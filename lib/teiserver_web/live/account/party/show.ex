34ce0b2c2c5621cdbd3f9b19e4cbb803284f1335
Teifion <teifion@teifion.co.uk>
2022-08-31 00:27:39 +0100

    Invites can now be created

--- lib/teiserver_web/live/account/party/show.ex
+++ lib/teiserver_web/live/account/party/show.ex
@@ -1,11 +1,12 @@
 defmodule TeiserverWeb.Account.PartyLive.Show do
   use TeiserverWeb, :live_view
-  # alias Phoenix.PubSub
+  alias Phoenix.PubSub
   require Logger
 
   alias Teiserver.Account
   alias Teiserver.Account.PartyLib
   import Central.Helpers.StringHelper, only: [possessive: 1]
+  import Central.Helpers.NumberHelper, only: [int_parse: 1]
 
   @impl true
   def mount(_params, session, socket) do
@@ -16,11 +17,15 @@ defmodule TeiserverWeb.Account.PartyLive.Show do
         |> NotificationPlug.live_call()
 
     client = Account.get_client_by_id(socket.assigns.user_id)
+    user = Account.get_user_by_id(socket.assigns.user_id)
+
+    :ok = PubSub.subscribe(Central.PubSub, "teiserver_client_messages:#{socket.assigns.user_id}")
 
     socket = socket
       |> add_breadcrumb(name: "Teiserver", url: "/teiserver")
       |> add_breadcrumb(name: "Parties", url: "/teiserver/account/parties")
       |> assign(:client, client)
+      |> assign(:user, user)
 
     {:ok, socket}
   end
@@ -60,10 +65,44 @@ defmodule TeiserverWeb.Account.PartyLive.Show do
     Phoenix.View.render(TeiserverWeb.Account.PartyLiveView, "show.html", assigns)
   end
 
+  @impl true
+  def handle_info(%{channel: "teiserver_client_messages:" <> _, event: :connected}, socket) do
+    {:noreply,
+      socket
+        |> assign(:client, Account.get_client_by_id(socket.assigns.user_id))
+    }
+  end
+
+  def handle_info(%{channel: "teiserver_client_messages:" <> _, event: :disconnected}, socket) do
+    {:noreply,
+      socket
+        |> assign(:client, nil)
+    }
+  end
+
+  def handle_info(%{channel: "teiserver_client_messages:" <> _}, socket) do
+    {:noreply, socket}
+  end
+
+  @impl true
+  def handle_event("create_invite", %{"userid" => userid_str}, socket) do
+    userid = int_parse(userid_str)
+    Account.create_party_invite(socket.assigns.party_id, userid)
+    {:noreply, socket}
+  end
+
   def build_user_lookup(socket) do
     existing_user_ids = Map.keys(socket.assigns.user_lookup)
 
-    new_users = (socket.assigns.party.members ++ socket.assigns.party.pending_invites)
+    ids = [
+      socket.assigns.party.members,
+      socket.assigns.party.pending_invites,
+      socket.assigns.user.friends
+    ]
+      |> List.flatten
+      |> Enum.uniq
+
+    new_users = ids
       |> Enum.reject(fn m -> Enum.member?(existing_user_ids, m) end)
       |> Account.list_users_from_cache
       |> Map.new(fn u -> {u.id, u} end)
