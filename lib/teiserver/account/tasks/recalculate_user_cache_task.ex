7ba47e7b7c967ce25a345a7e9981522d7dd48d46
Teifion <teifion@teifion.co.uk>
2023-02-09 18:00:09 +0000

    Extended post match value caching

--- lib/teiserver/account/tasks/recalculate_user_cache_task.ex
+++ lib/teiserver/account/tasks/recalculate_user_cache_task.ex
@@ -13,12 +13,49 @@ defmodule Teiserver.Account.RecacheUserStatsTask do
   @spec match_processed(map(), T.userid()) :: :ok
   def match_processed(match, userid) do
     case match.game_type do
+      "Duel" -> do_match_processed_duel(userid)
       "Team" -> do_match_processed_team(userid)
       _ -> :ok
     end
   end
 
-  defp do_match_processed_team(userid) do
+  def do_match_processed_duel(userid) do
+    filter_type_id = MatchRatingLib.rating_type_name_lookup()["Duel"]
+    logs = Game.list_rating_logs(
+      search: [
+        user_id: userid,
+        rating_type_id: filter_type_id,
+        inserted_after: Timex.now() |> Timex.shift(days: -31)
+      ],
+      order_by: "Newest first",
+      limit: 50,
+      preload: [:match, :match_membership]
+    )
+
+    win_count = logs
+      |> Enum.filter(fn log -> log.match_membership.win end)
+      |> Enum.count
+
+    loss_count = logs
+      |> Enum.reject(fn log -> log.match_membership.win end)
+      |> Enum.count
+
+    total = Enum.count(logs)
+
+    if total > 0 do
+      winrate = win_count / total
+
+      Account.update_user_stat(userid, %{
+        "win_count.duel" => win_count,
+        "loss_count.duel" => loss_count,
+        "win_rate.duel" => winrate |> percent(1)
+      })
+    end
+
+    :ok
+  end
+
+  def do_match_processed_team(userid) do
     filter_type_id = MatchRatingLib.rating_type_name_lookup()["Team"]
     logs = Game.list_rating_logs(
       search: [
@@ -30,6 +67,14 @@ defmodule Teiserver.Account.RecacheUserStatsTask do
       limit: 50,
       preload: [:match, :match_membership]
     )
+
+    win_count = logs
+      |> Enum.filter(fn log -> log.match_membership.win end)
+      |> Enum.count
+
+    loss_count = logs
+      |> Enum.reject(fn log -> log.match_membership.win end)
+      |> Enum.count
 
     statuses = logs
       |> Enum.group_by(fn log ->
@@ -44,12 +89,17 @@ defmodule Teiserver.Account.RecacheUserStatsTask do
     total = Enum.count(logs)
 
     if total > 0 do
+      winrate = win_count / total
+
       Account.update_user_stat(userid, %{
         "exit_status.team.count" => total,
         "exit_status.team.stayed" => (statuses[:stayed] || 0) / total |> percent(1),
         "exit_status.team.early" => (statuses[:early] || 0) / total |> percent(1),
         "exit_status.team.abandoned" => (statuses[:abandoned] || 0) / total |> percent(1),
         "exit_status.team.noshow" => (statuses[:noshow] || 0) / total |> percent(1),
+        "win_count.team" => win_count,
+        "loss_count.team" => loss_count,
+        "win_rate.team" => winrate |> percent(1)
       })
     end
 
