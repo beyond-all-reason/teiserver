7aab3fbf7fb309c69302c86e7785c55fb0c7ae73
Teifion <teifion@teifion.co.uk>
2023-03-12 15:04:14 +0000

    Moved logging links to ts admin

--- lib/teiserver/startup.ex
+++ lib/teiserver/startup.ex
@@ -64,7 +64,7 @@ defmodule Teiserver.Startup do
 
     # Permissions setup
     add_permission_set("teiserver", "admin", ~w(account battle clan queue))
-    add_permission_set("teiserver", "staff", ~w(reviewer moderator admin communication clan telemetry))
+    add_permission_set("teiserver", "staff", ~w(reviewer moderator admin communication clan telemetry server))
     add_permission_set("teiserver", "dev", ~w(infolog))
     add_permission_set("teiserver", "reports", ~w(client server match ratings infolog))
     add_permission_set("teiserver", "api", ~w(battle))
