844db1b7322ceca85f27b8af493dabe752e37dc7
Teifion <teifion@teifion.co.uk>
2023-03-12 15:17:49 +0000

    Removed comments

--- lib/central/account/schemas/user_token.ex
+++ lib/central/account/schemas/user_token.ex
@@ -24,21 +24,3 @@ defmodule Central.Account.UserToken do
   @spec authorize(any, Plug.Conn.t(), atom) :: boolean
   def authorize(_, conn, _), do: allow?(conn, "admin.dev")
 end
-
-# Central.Account.create_user_token(%{
-#   value: "value1",
-#   user_id: 3,
-#   user_agent: "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0",
-#   ip: "192.168.0.1",
-#   expires: Timex.shift(Timex.now(), days: 5),
-#   last_used: Timex.shift(Timex.now(), days: -5),
-# })
-
-# Central.Account.create_user_token(%{
-#   value: "value2",
-#   user_id: 3,
-#   user_agent: "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/110.0",
-#   ip: "192.168.0.1",
-#   expires: nil,
-#   last_used: Timex.shift(Timex.now(), days: -1),
-# })
