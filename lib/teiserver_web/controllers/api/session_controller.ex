20552eb2af699abab89a095e3810875917bc5b00
Teifion <teifion@teifion.co.uk>
2023-03-21 22:02:36 +0000

    Added registration API

--- lib/teiserver_web/controllers/api/session_controller.ex
+++ lib/teiserver_web/controllers/api/session_controller.ex
@@ -24,6 +24,109 @@ defmodule TeiserverWeb.API.SessionController do
     |> render("login.json", %{result: :failure, reason: "auth error"})
   end
 
+  @spec register(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def register(conn, %{"user" => user_params}) do
+    user_params = Account.merge_default_params(user_params)
+    config_setting = Central.Config.get_site_config_cache("user.Enable user registrations")
+
+    {allowed, reason} = cond do
+      config_setting == "Allowed" ->
+        {true, nil}
+
+      config_setting == "Disabled" ->
+        {false, "disabled"}
+
+      config_setting == "Link only" ->
+        code = Teiserver.Account.get_code(user_params["code"] || "!no_code!")
+
+        cond do
+          user_params["code"] == nil ->
+            {false, "no_code"}
+
+          code == nil ->
+            {false, "invalid_code"}
+
+          code.purpose != "user_registration" ->
+            {false, "invalid_code"}
+
+          Timex.compare(Timex.now(), code.expires) == 1 ->
+            {false, "expired_code"}
+
+          true ->
+            {true, nil}
+        end
+
+      true ->
+        {false, "disabled"}
+    end
+
+    existing_user = Account.get_user_by_email(user_params["email"])
+
+    result = cond do
+      allowed == false ->
+        {:error, "Not allowed because #{reason}"}
+
+      existing_user != nil ->
+        {:ok, existing_user}
+
+      user_params["name"] == nil ->
+        {:error, "Missing parameter 'name'"}
+
+      user_params["email"] == nil ->
+        {:error, "Missing parameter 'email'"}
+
+      user_params["password"] == nil ->
+        {:error, "Missing parameter 'password'"}
+
+      true ->
+        case Account.self_create_user(user_params) do
+          {:ok, user} ->
+            case Teiserver.Account.get_code(user_params["code"]) do
+              nil ->
+                :ok
+              code ->
+                add_audit_log(conn, "Account:User registration", %{
+                  code_value: code.value,
+                  code_creator: code.user_id
+                })
+            end
+
+            {:ok, user}
+
+          {:error, %Ecto.Changeset{} = changeset} ->
+            IO.puts ""
+            IO.inspect changeset
+            IO.puts ""
+            {:error, "Changeset error"}
+        end
+    end
+
+
+    conn
+    |> register_reply(result)
+  end
+
+  @spec register(Plug.Conn.t(), map()) :: Plug.Conn.t()
+  def register(conn, params) do
+    IO.puts ""
+    IO.inspect params
+    IO.puts ""
+
+    raise "X"
+  end
+
+  defp register_reply(conn, {:ok, user}) do
+    conn
+    |> put_status(200)
+    |> render("register.json", %{user: user})
+  end
+
+  defp register_reply(conn, {:error, reason}) do
+    conn
+    |> put_status(400)
+    |> render("register.json", %{result: :failure, reason: reason})
+  end
+
   @spec request_token(Plug.Conn.t(), Map.t()) :: Plug.Conn.t()
   def request_token(conn, %{"email" => email, "password" => raw_password} = params) do
     expires = case Map.get(params, "ttl", nil) do
--- lib/teiserver_web/views/api/session_view.ex
+++ lib/teiserver_web/views/api/session_view.ex
@@ -9,6 +9,14 @@ defmodule TeiserverWeb.API.SessionView do
     %{result: :failure, reason: reason}
   end
 
+  def render("register.json", %{user: user}) do
+    %{result: :success, userid: user.id}
+  end
+
+  def render("register.json", %{result: :failure, reason: reason}) do
+    %{result: :failure, reason: reason}
+  end
+
   def render("token.json", %{token_value: token_value}) do
     %{result: :success, token_value: token_value}
   end
