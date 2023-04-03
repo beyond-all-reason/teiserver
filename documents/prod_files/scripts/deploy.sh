7270ea9e1431eb08e64750d7db4c550c8954bbb2
Teifion <teifion@teifion.co.uk>
2023-04-03 11:59:56 +0100

    Updated docs

--- documents/prod_files/scripts/deploy.sh
+++ documents/prod_files/scripts/deploy.sh
@@ -44,6 +44,13 @@ echo "Reset logs"
 sudo chown -R deploy:deploy /apps/central
 sudo chown -R deploy:deploy /var/log/central
 
+# We found on a faster server if we started up the app really quickly it would generate
+# very high CPU load for no apparent reason, putting this in places solves it
+# if you are using a lower end VPS you can likely remove it (we only needed it when
+# we moved to a 5800X bare metal server, never had an issue while using a VPS).
+echo "Sleeping"
+sleep 5
+
 echo "Starting service"
 sudo systemctl restart central.service
 
