8be2152c5a39acb2f59ba8485a0397dcaa900168
Teifion <teifion@teifion.co.uk>
2023-04-24 23:40:10 +0100

    More bugfixes

--- lib/teiserver/protocols/tachyon_v1/lobby_out.ex
+++ lib/teiserver/protocols/tachyon_v1/lobby_out.ex
@@ -21,13 +21,6 @@ defmodule Teiserver.Protocols.Tachyon.V1.LobbyOut do
     }
   end
 
-  def do_reply(:closed, %{lobby_id: lobby_id}) do
-    %{
-      "cmd" => "s.lobby.closed",
-      "lobby_id" => lobby_id
-    }
-  end
-
   ###########
   # Query
   def do_reply(:query, lobby_list) do
@@ -82,6 +75,18 @@ defmodule Teiserver.Protocols.Tachyon.V1.LobbyOut do
 
   ###########
   # User Updates
+  def do_reply(:add_user, {_, %{lobby_id: lobby_id, client: client}}) do
+    user = Account.get_user_by_id(client.userid)
+
+    %{
+      "cmd" => "s.lobby.add_user",
+      "lobby_id" => lobby_id,
+      "joiner_id" => client.userid,
+      "client" => Tachyon.convert_object(client, :client),
+      "user" => Tachyon.convert_object(user, :user)
+    }
+  end
+
   def do_reply(:add_user, %{lobby_id: lobby_id, client: client}) do
     user = Account.get_user_by_id(client.userid)
 
@@ -91,6 +96,14 @@ defmodule Teiserver.Protocols.Tachyon.V1.LobbyOut do
       "joiner_id" => client.userid,
       "client" => Tachyon.convert_object(client, :client),
       "user" => Tachyon.convert_object(user, :user)
+    }
+  end
+
+  def do_reply(:remove_user, {_lobby_id, %{lobby_id: lobby_id, client: client}}) do
+    %{
+      "cmd" => "s.lobby.remove_user",
+      "lobby_id" => lobby_id,
+      "leaver_id" => client.userid
     }
   end
 
@@ -110,6 +123,14 @@ defmodule Teiserver.Protocols.Tachyon.V1.LobbyOut do
     }
   end
 
+  def do_reply(:kick_user, {_lobby_id, %{lobby_id: lobby_id, client: client}}) do
+    %{
+      "cmd" => "s.lobby.kick_user",
+      "lobby_id" => lobby_id,
+      "kicked_id" => client.userid
+    }
+  end
+
   def do_reply(:updated_client_battlestatus, {lobby_id, %{client: client, reason: reason}}) do
     %{
       "cmd" => "s.lobby.updated_client_battlestatus",
@@ -121,12 +142,29 @@ defmodule Teiserver.Protocols.Tachyon.V1.LobbyOut do
 
   ###########
   # General Updates
+  def do_reply(:updated, {_, %{lobby_id: lobby_id}}) do
+    lobby = Lobby.get_lobby(lobby_id)
+
+    %{
+      "cmd" => "s.lobby.updated",
+      "lobby" => Tachyon.convert_object(lobby, :lobby)
+    }
+  end
+
   def do_reply(:updated, %{lobby_id: lobby_id}) do
     lobby = Lobby.get_lobby(lobby_id)
 
     %{
       "cmd" => "s.lobby.updated",
       "lobby" => Tachyon.convert_object(lobby, :lobby)
+    }
+  end
+
+  def do_reply(:update_values, {_, %{lobby_id: lobby_id, changes: new_values}}) do
+    %{
+      "cmd" => "s.lobby.update_values",
+      "lobby_id" => lobby_id,
+      "new_values" => new_values
     }
   end
 
@@ -138,15 +176,7 @@ defmodule Teiserver.Protocols.Tachyon.V1.LobbyOut do
     }
   end
 
-  def do_reply(:update_values, {lobby_id, %{lobby_id: lobby_id, changes: new_values}}) do
-    %{
-      "cmd" => "s.lobby.update_values",
-      "lobby_id" => lobby_id,
-      "new_values" => new_values
-    }
-  end
-
-  def do_reply(:updated_queue, %{lobby_id: lobby_id, id_list: id_list}) do
+  def do_reply(:updated_queue, {_, %{lobby_id: lobby_id, id_list: id_list}}) do
     %{
       "cmd" => "s.lobby.updated_queue",
       "lobby_id" => lobby_id,
@@ -156,12 +186,29 @@ defmodule Teiserver.Protocols.Tachyon.V1.LobbyOut do
 
   ###########
   # Start area updates
+  def do_reply(:add_start_area, {_, %{lobby_id: lobby_id, area_id: area_id, area: area}}) do
+    %{
+      "cmd" => "s.lobby.add_start_area",
+      "lobby_id" => lobby_id,
+      "area_id" => area_id,
+      "structure" => area
+    }
+  end
+
   def do_reply(:add_start_area, %{lobby_id: lobby_id, area_id: area_id, area: area}) do
     %{
       "cmd" => "s.lobby.add_start_area",
       "lobby_id" => lobby_id,
       "area_id" => area_id,
       "structure" => area
+    }
+  end
+
+  def do_reply(:remove_start_area, {_, %{lobby_id: lobby_id, area_id: area_id}}) do
+    %{
+      "cmd" => "s.lobby.remove_start_area",
+      "lobby_id" => lobby_id,
+      "area_id" => area_id
     }
   end
 
@@ -304,7 +351,7 @@ defmodule Teiserver.Protocols.Tachyon.V1.LobbyOut do
 
   ###########
   # Modoptions
-  def do_reply(:set_modoption, {lobby_id, msg}) do
+  def do_reply(:set_modoption, {_lobby_id, msg}) do
     %{
       "cmd" => "s.lobby.set_modoptions",
       "lobby_id" => msg.lobby_id,
@@ -324,7 +371,7 @@ defmodule Teiserver.Protocols.Tachyon.V1.LobbyOut do
     }
   end
 
-  def do_reply(:set_modoptions, {lobby_id, msg}) do
+  def do_reply(:set_modoptions, {_lobby_id, msg}) do
     %{
       "cmd" => "s.lobby.set_modoptions",
       "lobby_id" => msg.lobby_id,
@@ -350,9 +397,25 @@ defmodule Teiserver.Protocols.Tachyon.V1.LobbyOut do
 
   ###########
   # Bots
+  def do_reply(:add_bot, {_, msg}) do
+    %{
+      "cmd" => "s.lobby.add_bot",
+      # "lobby_id" => msg.lobby_id,
+      "bot" => msg.bot
+    }
+  end
+
   def do_reply(:add_bot, msg) do
     %{
       "cmd" => "s.lobby.add_bot",
+      # "lobby_id" => msg.lobby_id,
+      "bot" => msg.bot
+    }
+  end
+
+  def do_reply(:update_bot, {_, msg}) do
+    %{
+      "cmd" => "s.lobby.update_bot",
       # "lobby_id" => msg.lobby_id,
       "bot" => msg.bot
     }
@@ -363,6 +426,14 @@ defmodule Teiserver.Protocols.Tachyon.V1.LobbyOut do
       "cmd" => "s.lobby.update_bot",
       # "lobby_id" => msg.lobby_id,
       "bot" => msg.bot
+    }
+  end
+
+  def do_reply(:remove_bot, {_, msg}) do
+    %{
+      "cmd" => "s.lobby.remove_bot",
+      # "lobby_id" => msg.lobby_id,
+      "bot_name" => msg.bot
     }
   end
 
