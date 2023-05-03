4c73f45609db6bc93aecbc18990559ab93c6c0e4
Charles Bock <charles@charlesbock.com>
2023-05-03 12:35:22 -0700

    Fix case sensitive email field and add tests

--- lib/teiserver/account/caches/user_cache.ex
+++ lib/teiserver/account/caches/user_cache.ex
@@ -71,7 +71,7 @@ defmodule Teiserver.Account.UserCache do
         user =
           Account.get_user(nil,
             search: [
-              email: email
+              email_lower: email
             ],
             select: [:id]
           )
