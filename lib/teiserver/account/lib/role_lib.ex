6d76149be00f49fd305ee641ddd9a14bd88d58b7
Teifion <teifion@teifion.co.uk>
2023-05-19 00:50:54 +0100

    Fixed role def object

--- lib/teiserver/account/lib/role_lib.ex
+++ lib/teiserver/account/lib/role_lib.ex
@@ -3,53 +3,57 @@ defmodule Teiserver.Account.RoleLib do
   A library with all the hard-coded data regarding user roles.
   """
 
-  @raw_role_data %{
+  # If Role A contains Role B, Role B needs to be listed first
+  @raw_role_data [
     # Global
-    "Default" => %{colour: "#666666", icon: "fa-solid fa-user", contains: ~w(), badge: false},
-    "Armada" => %{colour: "#000066", icon: "fa-solid fa-a", contains: ~w(), badge: false},
-    "Cortex" => %{colour: "#660000", icon: "fa-solid fa-c", contains: ~w(), badge: false},
-    "Legion" => %{colour: "#006600", icon: "fa-solid fa-l", contains: ~w(), badge: false},
-    "Raptor" => %{colour: "#AA6600", icon: "fa-solid fa-drumstick", contains: ~w(), badge: false},
-    "Scavenger" => %{colour: "#660066", icon: "fa-solid fa-user-robot", contains: ~w(), badge: false},
+    %{name: "Default", colour: "#666666", icon: "fa-solid fa-user", contains: ~w(), badge: false},
+    %{name: "Armada", colour: "#000066", icon: "fa-solid fa-a", contains: ~w(), badge: false},
+    %{name: "Cortex", colour: "#660000", icon: "fa-solid fa-c", contains: ~w(), badge: false},
+    %{name: "Legion", colour: "#006600", icon: "fa-solid fa-l", contains: ~w(), badge: false},
+    %{name: "Raptor", colour: "#AA6600", icon: "fa-solid fa-drumstick", contains: ~w(), badge: false},
+    %{name: "Scavenger", colour: "#660066", icon: "fa-solid fa-user-robot", contains: ~w(), badge: false},
+
+    # Property
+    %{name: "Trusted", colour: "#000000", icon: "fa-duotone fa-check", contains: ~w()},
+    %{name: "Bot", colour: "#777777", icon: "fa-solid fa-user-robot", contains: ~w()},
+    %{name: "Verified", colour: "#66AA66", icon: "fa-duotone fa-check", contains: ~w(), badge: false},
 
     # Authority
-    "Bot" => %{colour: "#777777", icon: "fa-solid fa-user-robot", contains: ~w()},
-    "Server" => %{colour: "#AA2088", icon: "fa-solid fa-user-gear", contains: ~w(Admin)},
-    "Admin" => %{colour: "#204A88", icon: "fa-solid fa-user-tie", contains: ~w(Moderator)},
-    "Moderator" => %{colour: "#FFAA00", icon: "fa-duotone fa-gavel", contains: ~w(Reviewer)},
-    "Reviewer" => %{colour: "#AA7700", icon: "fa-duotone fa-user-magnifying-glass", contains: ~w(Overwatch)},
-    "Overwatch" => %{colour: "#AA7733", icon: "fa-duotone fa-clipboard-list-check", contains: ~w()},
+    %{name: "Overwatch", colour: "#AA7733", icon: "fa-duotone fa-clipboard-list-check", contains: ~w()},
+    %{name: "Reviewer", colour: "#AA7700", icon: "fa-duotone fa-user-magnifying-glass", contains: ~w(Overwatch)},
+    %{name: "Moderator", colour: "#FFAA00", icon: "fa-duotone fa-gavel", contains: ~w(Reviewer)},
+    %{name: "Admin", colour: "#204A88", icon: "fa-solid fa-user-tie", contains: ~w(Moderator)},
+    %{name: "Server", colour: "#AA2088", icon: "fa-solid fa-user-gear", contains: ~w(Admin)},
 
     # Staff
-    "Core" => %{colour: "#008800", icon: "fa-duotone fa-code-branch", contains: ~w(Contributor)},
-    "Contributor" => %{colour: "#00AA66", icon: "fa-duotone fa-code-commit", contains: ~w(Trusted)},
-    "GDT" => %{colour: "#AA0000", icon: "fa-duotone fa-pen-ruler", contains: ~w()},
-    "Tester" => %{colour: "#00AAAA", icon: "fa-duotone fa-vial", contains: ~w()},
+    %{name: "Tester", colour: "#00AAAA", icon: "fa-duotone fa-vial", contains: ~w()},
+    %{name: "GDT", colour: "#AA0000", icon: "fa-duotone fa-pen-ruler", contains: ~w()},
+    %{name: "Contributor", colour: "#00AA66", icon: "fa-duotone fa-code-commit", contains: ~w(Trusted)},
+    %{name: "Core", colour: "#008800", icon: "fa-duotone fa-code-branch", contains: ~w(Contributor)},
 
     # Privileged
-    "VIP" => %{colour: "#AA8833", icon: "fa-duotone fa-sparkles", contains: ~w()},
-    "Caster" => %{colour: "#660066", icon: "fa-duotone fa-microphone-lines", contains: ~w(Streamer Tournament)},
-    "Donor" => %{colour: "#0066AA", icon: "fa-duotone fa-euro", contains: ~w()},
-    "Streamer" => %{colour: "#0066AA", icon: "fa-brands fa-twitch", contains: ~w()},
-    "Tournament" => %{colour: "#0000AA", icon: "fa-duotone fa-trophy", contains: ~w()},
-    "Trusted" => %{colour: "#000000", icon: "fa-duotone fa-check", contains: ~w()},
-    "Verified" => %{colour: "#66AA66", icon: "fa-duotone fa-check", contains: ~w(), badge: false},
-  }
-
-  # Given a role name it returns the list of roles (recursively) it contains
-  @spec build_contains_map(String.t()) :: [String.t()]
-  defp build_contains_map(name) do
-    @raw_role_data[name]
-      |> Map.get(:contains, [])
-      |> Enum.map(fn r -> build_contains_map(name) end)
-  end
+    %{name: "VIP", colour: "#AA8833", icon: "fa-duotone fa-sparkles", contains: ~w()},
+    %{name: "Streamer", colour: "#0066AA", icon: "fa-brands fa-twitch", contains: ~w()},
+    %{name: "Tournament", colour: "#0000AA", icon: "fa-duotone fa-trophy", contains: ~w()},
+    %{name: "Caster", colour: "#660066", icon: "fa-duotone fa-microphone-lines", contains: ~w(Streamer Tournament)},
+    %{name: "Donor", colour: "#0066AA", icon: "fa-duotone fa-euro", contains: ~w()},
+  ]
 
   @role_data @raw_role_data
-    # |> Map.new(fn {name, role} ->
-    #   role = Map.merge(role, %{
-    #     contains: build_contains_map(name)
-    #   })
-    # end)
+    |> Enum.reduce(%{}, fn (role_def, temp_result) ->
+      extra_contains = role_def.contains
+        |> Enum.map(fn c -> [c | temp_result[c].contains] end)
+        |> List.flatten
+        |> Enum.uniq
+
+      new_def = role_def
+        |> Map.merge(%{
+          contains: extra_contains
+        })
+
+      Map.put(temp_result, new_def.name, new_def)
+    end)
+    |> Map.new()
 
   @spec role_data() :: map()
   def role_data() do
