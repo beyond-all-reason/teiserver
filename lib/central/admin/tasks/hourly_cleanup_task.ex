de96aa50386a0f69a99e8a4f9442ea5b5406cc75
Teifion <teifion@teifion.co.uk>
2023-06-13 23:04:03 +0100

    Swapped to raw query

--- lib/central/admin/tasks/hourly_cleanup_task.ex
+++ lib/central/admin/tasks/hourly_cleanup_task.ex
@@ -2,16 +2,16 @@ defmodule Central.Admin.HourlyCleanupTask do
   @moduledoc false
   use Oban.Worker, queue: :cleanup
 
-  alias Teiserver.Account
+  alias Central.Repo
+  import Central.Helpers.TimexHelper, only: [date_to_str: 2]
 
   @impl Oban.Worker
   @spec perform(any) :: :ok
   def perform(_) do
-    # First, find and remove all expired codes
-    Account.list_codes(search: [expired: true])
-    |> Enum.each(fn c ->
-      Account.delete_code(c)
-    end)
+    before_timestamp = Timex.now()
+      |> date_to_str(format: :ymd_hms)
+
+    Ecto.Adapters.SQL.query(Repo, "DELETE FROM account_codes WHERE expires < '#{before_timestamp}'", [])
 
     :ok
   end
