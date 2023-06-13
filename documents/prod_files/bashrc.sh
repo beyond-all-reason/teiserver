f7fe71074f3a3623650f5fb01b58e8419275b469
Teifion <teifion@teifion.co.uk>
2023-06-13 23:04:09 +0100

    Added postgres log alias

--- documents/prod_files/bashrc.sh
+++ documents/prod_files/bashrc.sh
@@ -1,13 +1,16 @@
 # Teiserver
 alias nginxlog='sudo grc tail /var/log/nginx/error.log -n 60 -f'
-alias siteloggrc='grc --config=elixir.log tail -f /var/log/central/error.log -n 60'
-alias sitelogtail='tail -f /var/log/central/error.log -n 60'
-alias siteinfogrc='grc --config=elixir.log tail -f /var/log/central/info.log -n 60'
-alias siteinfotail='tail -f /var/log/central/info.log -n 60'
-alias centralapp='sh /apps/central/bin/central'
+alias sitelog='grc --config=elixir.log tail -f /var/log/central/error.log -n 60'
+alias siteinfo='grc --config=elixir.log tail -f /var/log/central/info.log -n 60'
 alias dodeploy='sudo sh /scripts/deploy.sh'
+
+alias catsiteinfo='cat /var/log/central/info.log'
+alias centralapp='sh /apps/central/bin/central'
+
 alias stable_deploy='sudo sh /scripts/stable_deploy.sh'
 alias preparedeploy='sudo sh /scripts/prepare_deploy.sh'
 alias quickrestart='sudo sh /scripts/quick_restart.sh'
+
+alias postgreslog='sudo cat /var/log/postgresql/postgresql-15-main.log'
 
 export PATH
