3a748e0f02a33113480bd97a42ab29e6f3fcb1ee
Teifion <teifion@teifion.co.uk>
2023-07-03 16:31:44 +0100

    More permission tweaks

--- lib/teiserver/communication/schemas/text_callback.ex
+++ lib/teiserver/communication/schemas/text_callback.ex
@@ -35,5 +35,5 @@ defmodule Teiserver.Communication.TextCallback do
   @spec authorize(atom, Plug.Conn.t(), Map.t()) :: boolean
   def authorize(:index, conn, _params), do: allow_any?(conn, ["Overwatch", "Contributor"])
   def authorize(:show, conn, _params), do: allow_any?(conn, ["Overwatch", "Contributor"])
-  def authorize(_action, conn, _params), do: allow?(conn, "Server")
+  def authorize(_action, conn, _params), do: allow?(conn, "Admin")
 end
