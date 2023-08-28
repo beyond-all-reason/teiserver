f76eea914777af717a78b33b8151addc70e6d0c9
Teifion <teifion@teifion.co.uk>
2023-08-28 12:39:20 +0100

    DB design bugfix

--- priv/repo/migrations/20230731152333_create_relationships_table.exs
+++ priv/repo/migrations/20230731152333_create_relationships_table.exs
@@ -3,8 +3,8 @@ defmodule Teiserver.Repo.Migrations.CreateRelationshipsTable do
 
   def change do
     create table(:account_relationships, primary_key: false) do
-      add :from_user_id, references(:account_users, on_delete: :nothing, primary_key: true)
-      add :to_user_id, references(:account_users, on_delete: :nothing, primary_key: true)
+      add :from_user_id, references(:account_users, on_delete: :nothing), primary_key: true
+      add :to_user_id, references(:account_users, on_delete: :nothing), primary_key: true
 
       add :state, :string
 
@@ -18,15 +18,15 @@ defmodule Teiserver.Repo.Migrations.CreateRelationshipsTable do
     create(unique_index(:account_relationships, [:from_user_id, :to_user_id]))
 
     create table(:account_friends, primary_key: false) do
-      add :user1_id, references(:account_users, on_delete: :nothing, primary_key: true)
-      add :user2_id, references(:account_users, on_delete: :nothing, primary_key: true)
+      add :user1_id, references(:account_users, on_delete: :nothing), primary_key: true
+      add :user2_id, references(:account_users, on_delete: :nothing), primary_key: true
 
       timestamps()
     end
 
     create table(:account_friend_requests, primary_key: false) do
-      add :from_user_id, references(:account_users, on_delete: :nothing, primary_key: true)
-      add :to_user_id, references(:account_users, on_delete: :nothing, primary_key: true)
+      add :from_user_id, references(:account_users, on_delete: :nothing), primary_key: true
+      add :to_user_id, references(:account_users, on_delete: :nothing), primary_key: true
 
       timestamps()
     end
