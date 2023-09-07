3589c77d73cd4c469a6b1926241ef4fe14628374
Dominik Stańczak-Marikin <stanczakdominik@gmail.com>
2023-09-07 08:53:01 +0200

    Further smurf routing adjustments

--- lib/central_web/router.ex
+++ lib/central_web/router.ex
@@ -558,6 +558,15 @@ defmodule TeiserverWeb.Router do
     get("/report_form/success", ReportFormController, :success)
     post("/report_form", ReportFormController, :create)
     get("/report_form/:id", ReportFormController, :index)
+
+    get("/users/smurf_search/:id", UserController, :smurf_search)
+
+    put("/users/mark_as_smurf_of/:smurf_id/:origin_id", UserController, :mark_as_smurf_of)
+    delete("/users/cancel_smurf_mark/:user_id", UserController, :cancel_smurf_mark)
+
+    get("/users/smurf_merge_form/:from_id/:to_id", UserController, :smurf_merge_form)
+    post("/users/smurf_merge_post/:from_id/:to_id", UserController, :smurf_merge_post)
+    delete("/users/delete_smurf_key/:id", UserController, :delete_smurf_key)
   end
 
   scope "/admin", TeiserverWeb.Admin, as: :admin do
@@ -634,14 +643,7 @@ defmodule TeiserverWeb.Router do
     get("/users/relationships/:id", UserController, :relationships)
     get("/users/action/:id/:action", UserController, :perform_action)
     put("/users/action/:id/:action", UserController, :perform_action)
-    get("/users/smurf_search/:id", UserController, :smurf_search)
 
-    put("/users/mark_as_smurf_of/:smurf_id/:origin_id", UserController, :mark_as_smurf_of)
-    delete("/users/cancel_smurf_mark/:user_id", UserController, :cancel_smurf_mark)
-
-    get("/users/smurf_merge_form/:from_id/:to_id", UserController, :smurf_merge_form)
-    post("/users/smurf_merge_post/:from_id/:to_id", UserController, :smurf_merge_post)
-    delete("/users/delete_smurf_key/:id", UserController, :delete_smurf_key)
     get("/users/full_chat/:id", UserController, :full_chat)
     get("/users/full_chat/:id/:page", UserController, :full_chat)
     get("/users/search", UserController, :index)
