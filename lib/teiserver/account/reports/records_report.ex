587e5bce9fcfe8494f143814b9c6d54f7bdd173b
Teifion <teifion@teifion.co.uk>
2023-09-24 21:44:43 +0100

    Improved the records report

--- lib/teiserver/account/reports/records_report.ex
+++ lib/teiserver/account/reports/records_report.ex
@@ -1,6 +1,7 @@
 defmodule Teiserver.Account.RecordsReport do
   alias Teiserver.Repo
   alias Teiserver.Logging
+  alias Teiserver.Helper.DatePresets
 
   @spec icon() :: String.t()
   def icon(), do: "fa-solid fa-trophy"
@@ -11,18 +12,19 @@ defmodule Teiserver.Account.RecordsReport do
   @top_count 3
 
   @spec run(Plug.Conn.t(), map()) :: {nil, map()}
-  def run(_conn, _params) do
+  def run(_conn, params) do
+    params = default_params(params)
+
     records = [
-      {"Peak users", get_top(~w(aggregates stats peak_user_counts total))},
-      {"Peak players", get_top(~w(aggregates stats peak_user_counts player))},
-      {"Unique users", get_top(~w(aggregates stats unique_users))},
-      {"Unique players", get_top(~w(aggregates stats unique_players))},
-      {"Total time (days)", get_top(~w(aggregates minutes total)) |> minutes_to_days},
-      {"Player time (days)", get_top(~w(aggregates minutes player)) |> minutes_to_days}
+      {"Peak users", get_top(~w(aggregates stats peak_user_counts total), params)},
+      {"Peak players", get_top(~w(aggregates stats peak_user_counts player), params)},
+      {"Unique users", get_top(~w(aggregates stats unique_users), params)},
+      {"Unique players", get_top(~w(aggregates stats unique_players), params)},
+      {"Total time (days)", get_top(~w(aggregates minutes total), params) |> minutes_to_days},
+      {"Player time (days)", get_top(~w(aggregates minutes player), params) |> minutes_to_days}
     ]
 
-    # force_recache = (Map.get(params, "recache", false) == "true")
-    force_recache = false
+    force_recache = (Map.get(params, "recache", false) == "true")
     today = Logging.get_todays_server_log(force_recache)
 
     today_data = %{
@@ -36,28 +38,38 @@ defmodule Teiserver.Account.RecordsReport do
     }
 
     %{
+      presets: DatePresets.long_ranges(),
       top_count: @top_count,
       records: records,
+      params: params,
       today: today_data
     }
   end
 
-  defp get_top(fields) do
+  defp get_top(fields, params) do
     path =
       fields
       |> Enum.map_join(" -> ", fn f -> "'#{f}'" end)
+
+    {start_date, _end_date} =
+      DatePresets.parse(
+        params["date_preset"],
+        "",
+        ""
+      )
 
     query = """
       SELECT
         logs.date,
         logs.data -> #{path}
       FROM teiserver_server_day_logs logs
+      WHERE logs.date > $2
       ORDER BY
         (logs.data -> #{path}) DESC
       LIMIT $1
     """
 
-    case Ecto.Adapters.SQL.query(Repo, query, [@top_count]) do
+    case Ecto.Adapters.SQL.query(Repo, query, [@top_count, start_date]) do
       {:ok, results} ->
         results.rows
 
@@ -75,5 +87,11 @@ defmodule Teiserver.Account.RecordsReport do
     |> Enum.map(fn row ->
       minutes_to_days_cell(row)
     end)
+  end
+
+  def default_params(params) do
+    Map.merge(%{
+      "date_preset" => "Last 12 months"
+    }, params["report"])
   end
 end
