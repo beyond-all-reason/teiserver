4e79eaa3cf5b4b96601568aeab833d859df06c13
Teifion <teifion@teifion.co.uk>
2023-09-26 17:54:29 +0100

    Bugfix

--- lib/central/account/startup.ex
+++ lib/central/account/startup.ex
@@ -3,11 +3,6 @@ defmodule Central.Account.Startup do
   use CentralWeb, :startup
 
   def startup do
-    QuickAction.add_items([
-      %{label: "Logout", icons: ["fa-regular fa-sign-out"], url: "/logout"},
-      %{label: "Edit account", icons: ["fa-regular fa-lock", :edit], url: "/account/edit"}
-    ])
-
     add_audit_types([
       "Account:User password reset",
       "Account:Failed login",
--- lib/teiserver/logging/lib/startup.ex
+++ lib/teiserver/logging/lib/startup.ex
@@ -8,32 +8,5 @@ defmodule Teiserver.Logging.Startup do
     add_permission_set("logging", "audit", ~w(show delete report))
     add_permission_set("logging", "error", ~w(show delete report))
     add_permission_set("logging", "live", ~w(show))
-
-    QuickAction.add_items([
-      %{
-        label: "Live view dashboard",
-        icons: ["fa-regular fa-tachometer-alt", :list],
-        url: "/logging/live/dashboard",
-        permissions: "logging.live"
-      },
-      %{
-        label: "Audit logs",
-        icons: [Teiserver.Logging.AuditLogLib.icon(), :list],
-        url: "/logging/audit",
-        permissions: "logging.audit"
-      },
-      %{
-        label: "Aggregate logs",
-        icons: [Teiserver.Logging.AggregateViewLogLib.icon(), :list],
-        url: "/logging/aggregate_views",
-        permissions: "logging.agggregate"
-      },
-      %{
-        label: "Page view logs",
-        icons: [Teiserver.Logging.PageViewLogLib.icon(), :list],
-        url: "/logging/page_views",
-        permissions: "logging.page_view"
-      }
-    ])
   end
 end
