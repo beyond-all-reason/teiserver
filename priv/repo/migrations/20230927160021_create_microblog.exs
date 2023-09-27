e6a77f9d2390dd53b13015eb44e0ff381172d9ba
Teifion <teifion@teifion.co.uk>
2023-09-27 21:46:33 +0100

    Created database migration

---
+++ priv/repo/migrations/20230927160021_create_microblog.exs
@@ -0,0 +1,25 @@
+defmodule Teiserver.Repo.Migrations.CreateMicroblog do
+  use Ecto.Migration
+
+  def change do
+    create table(:microblog_tags) do
+      add :name, :string
+      add :colour, :string
+      add :icon, :string
+    end
+
+    create table(:microblog_posts) do
+      add :poster_id, references(:account_users, on_delete: :nothing)
+
+      add :title, :string
+      add :contents, :text
+
+      timestamps()
+    end
+
+    create table(:microblog_post_tags, primary_key: false) do
+      add :post_id, references(:microblog_posts, on_delete: :nothing), primary_key: true
+      add :tag_id, references(:microblog_tags, on_delete: :nothing), primary_key: true
+    end
+  end
+end
