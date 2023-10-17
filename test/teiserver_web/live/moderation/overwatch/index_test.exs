86723b581421ef9fa0dc848e04e0280a3ebb7c71
Teifion <teifion@teifion.co.uk>
2023-10-17 21:42:16 +0100

    Extended report group tests

--- test/teiserver_web/live/moderation/overwatch/index_test.exs
+++ test/teiserver_web/live/moderation/overwatch/index_test.exs
@@ -29,4 +29,18 @@ defmodule TeiserverWeb.Moderation.Overwatch.IndexLiveTest do
       assert html =~ "#{target_user.name}"
     end
   end
+
+  describe "Report group" do
+    setup [:auth_setup]
+    test "show 1", %{conn: conn, user: conn_user} do
+      target_user = TeiserverTestLib.new_user()
+      {:ok, rg, _report} = TeiserverTestLib.create_moderation_user_report(target_user.id, conn_user.id, %{extra_text: "#{__MODULE__} extra text"})
+
+      {:ok, _index_live, html} = live(conn, ~p"/moderation/overwatch/report_group/#{rg.id}")
+      assert html =~ "Report group for #{target_user.name}"
+      assert html =~ "#{__MODULE__} extra text"
+      assert html =~ "Reports (1)"
+      assert html =~ "Actions (0)"
+    end
+  end
 end
