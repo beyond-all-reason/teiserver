c0da503099502bf80545d5f6ec51b208fc734b17
Teifion <teifion@teifion.co.uk>
2023-10-25 00:17:54 +0100

    Filled out microblog report

--- lib/teiserver/communication/reports/microblog_report.ex
+++ lib/teiserver/communication/reports/microblog_report.ex
@@ -1,16 +1,31 @@
 defmodule Teiserver.Communication.MicroblogReport do
   @moduledoc false
+  alias Teiserver.Microblog
 
   @spec icon() :: String.t()
   def icon(), do: Teiserver.Microblog.PostLib.icon()
 
   @spec permissions() :: String.t()
-  def permissions(), do: "Reviewer"
+  def permissions(), do: "Server"
 
   @spec run(Plug.Conn.t(), map()) :: map
   def run(_conn, params) do
+    params = default_params(params)
+
+    posts = Microblog.list_posts(
+      order_by: params["order_by"],
+      preload: [:poster]
+    )
+
     %{
+      posts: posts,
       params: params
     }
+  end
+
+  defp default_params(params) do
+    Map.merge(%{
+      "order_by" => "Newest first"
+    }, params)
   end
 end
