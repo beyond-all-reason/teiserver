59df4ef5b867eb9b15c34222a4de89d53480a4e7
Teifion <teifion@teifion.co.uk>
2023-10-29 19:00:14 +0000

    Changed home page test to liveview test

---
+++ test/teiserver_web/live/general/home/index_live_test.exs
@@ -0,0 +1,29 @@
+defmodule TeiserverWeb.General.Home.IndexLiveTest do
+  @moduledoc false
+  use CentralWeb.ConnCase
+
+  import Phoenix.LiveViewTest
+
+  defp auth_setup(_) do
+    Central.Helpers.GeneralTestLib.conn_setup()
+    |> Teiserver.TeiserverTestLib.conn_setup()
+  end
+
+  describe "Anon" do
+    test "index", %{conn: conn} do
+      {:error, {:redirect, resp}} = live(conn, ~p"/")
+      assert resp == %{flash: %{"error" => "You must log in to access this page."}, to: ~p"/login"}
+    end
+  end
+
+  describe "Auth" do
+    setup [:auth_setup]
+
+    test "index", %{conn: conn} do
+      {:ok, _index_live, html} = live(conn, ~p"/")
+
+      assert html =~ "Logout"
+      assert html =~ "Notifications"
+    end
+  end
+end
