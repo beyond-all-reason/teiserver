e8ba1315b583be07b9998e85cad6df43fd88a4c6
Teifion <teifion@teifion.co.uk>
2023-10-30 20:02:22 +0000

    Reverted bad removal

---
+++ lib/central_web/views/account/user_view.ex
@@ -0,0 +1,12 @@
+defmodule CentralWeb.Account.UserView do
+  use CentralWeb, :view
+
+  @spec view_colour :: atom
+  def view_colour, do: :success
+
+  @spec icon :: String.t()
+  def icon, do: "fa-solid fa-user"
+
+  @spec name :: String.t()
+  def name, do: "user"
+end
---
+++ lib/teiserver_web/live_components/user_picker.ex
@@ -0,0 +1,10 @@
+defmodule TeiserverWeb.Account.LiveComponents.UserPicker do
+  # In Phoenix apps, the line is typically: use MyAppWeb, :live_component
+  use CentralWeb, :live_component
+
+  def render(assigns) do
+    ~H"""
+    <div class="hero"><%= @content %></div>
+    """
+  end
+end
