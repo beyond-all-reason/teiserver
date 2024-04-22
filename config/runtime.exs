0362cf0d47200ca98be1d3b0527b3f4bb2a09c99
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2024-04-22 20:54:44 +0100

    Allow email to be deactivated at startup

--- config/runtime.exs
+++ config/runtime.exs
@@ -94,34 +94,37 @@ if config_env() == :prod do
     issuer: Teiserver.ConfigHelpers.get_env("GUARDIAN_ISSUER", "teiserver"),
     secret_key: Teiserver.ConfigHelpers.get_env("GUARDIAN_SECRET_KEY")
 
+  if Teiserver.ConfigHelpers.get_env("ENABLE_EMAIL_INTEGRATION", :bool) do
+    smtp_server = Teiserver.ConfigHelpers.get_env("SMTP_HOSTNAME")
 
-  config :teiserver, Teiserver.Mailer,
-    adapter: Bamboo.SMTPAdapter,
-    contact_address:
-      Teiserver.ConfigHelpers.get_env("CONTACT_EMAIL_ADDRESS", "info@#{domain_name}"),
-    noreply_name: "Beyond All Reason",
-    noreply_address:
-      Teiserver.ConfigHelpers.get_env("NOREPLY_EMAIL_ADDRESS", "noreply@#{domain_name}"),
-    smtp_server: Teiserver.ConfigHelpers.get_env("SMTP_SERVER"),
-    hostname: Teiserver.ConfigHelpers.get_env("SMTP_HOSTNAME"),
-    # port: 1025,
-    port: Teiserver.ConfigHelpers.get_env("SMTP_PORT", "587", :int),
-    username: Teiserver.ConfigHelpers.get_env("SMTP_USERNAME"),
-    password: Teiserver.ConfigHelpers.get_env("SMTP_PASSWORD"),
-    # tls: :if_available, # can be `:always` or `:never`
-    # can be `:always` or `:never`
-    tls: :always,
-    tls_verify:
-      if(Teiserver.ConfigHelpers.get_env("SMTP_TLS_VERIFY", true, :bool),
-        do: :verify_peer,
-        else: :verify_none
-      ),
-    # or {":system", ALLOWED_TLS_VERSIONS"} w/ comma seprated values (e.g. "tlsv1.1,tlsv1.2")
-    allowed_tls_versions: [:"tlsv1.2"],
-    # can be `true`
-    no_mx_lookups: false,
-    # auth: :if_available # can be `always`. If your smtp relay requires authentication set it to `always`.
-    auth: :always
+    config :teiserver, Teiserver.Mailer,
+      adapter: Bamboo.SMTPAdapter,
+      contact_address:
+        Teiserver.ConfigHelpers.get_env("CONTACT_EMAIL_ADDRESS", "info@#{domain_name}"),
+      noreply_name: "Beyond All Reason",
+      noreply_address:
+        Teiserver.ConfigHelpers.get_env("NOREPLY_EMAIL_ADDRESS", "noreply@#{domain_name}"),
+      smtp_server: Teiserver.ConfigHelpers.get_env("SMTP_SERVER"),
+      hostname: Teiserver.ConfigHelpers.get_env("SMTP_HOSTNAME"),
+      # port: 1025,
+      port: Teiserver.ConfigHelpers.get_env("SMTP_PORT", "587", :int),
+      username: Teiserver.ConfigHelpers.get_env("SMTP_USERNAME"),
+      password: Teiserver.ConfigHelpers.get_env("SMTP_PASSWORD"),
+      # tls: :if_available, # can be `:always` or `:never`
+      # can be `:always` or `:never`
+      tls: :always,
+      tls_verify:
+        if(Teiserver.ConfigHelpers.get_env("SMTP_TLS_VERIFY", true, :bool),
+          do: :verify_peer,
+          else: :verify_none
+        ),
+      # or {":system", ALLOWED_TLS_VERSIONS"} w/ comma seprated values (e.g. "tlsv1.1,tlsv1.2")
+      allowed_tls_versions: [:"tlsv1.2"],
+      # can be `true`
+      no_mx_lookups: false,
+      # auth: :if_available # can be `always`. If your smtp relay requires authentication set it to `always`.
+      auth: :always
+  end
 
   log_root_path = Teiserver.ConfigHelpers.get_env("LOG_ROOT_PATH", "/var/log/teiserver/")
 
