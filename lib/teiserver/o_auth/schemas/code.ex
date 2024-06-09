d2b32cded09d31add382d4d7a7eca4a32eecc801
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2024-06-09 11:10:30 +0100

    Authorization code logic and table

---
+++ lib/teiserver/o_auth/schemas/code.ex
@@ -0,0 +1,33 @@
+defmodule Teiserver.OAuth.Code do
+  @moduledoc false
+  use TeiserverWeb, :schema
+
+  alias Teiserver.OAuth
+
+  @type t :: %__MODULE__{
+          value: String.t(),
+          owner: User.t(),
+          application: OAuth.Application.t(),
+          scopes: OAuth.Application.scopes(),
+          expires_at: DateTime.t()
+        }
+
+  schema "oauth_codes" do
+    field :value, :string
+    belongs_to :owner, Teiserver.Account.User, primary_key: true
+    belongs_to :application, OAuth.Application, primary_key: true
+    field :scopes, {:array, :string}
+    field :expires_at, :utc_datetime
+
+    timestamps()
+  end
+
+  def changeset(code, attrs) do
+    attrs = attrs |> uniq_lists(~w(scopes)a)
+
+    code
+    |> cast(attrs, [:value, :owner_id, :application_id, :scopes, :expires_at])
+    |> validate_required([:value, :owner_id, :application_id, :scopes, :expires_at])
+    |> Ecto.Changeset.validate_subset(:scopes, OAuth.Application.allowed_scopes())
+  end
+end
