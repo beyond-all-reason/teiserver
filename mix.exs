f9a2ccb183b9ac090c2051dec829fe1b1f71f60f
Joshua Augustinus <jauggy@gmail.com>
2024-07-10 18:20:59 +1000

    Minor fixes to pass automated checks

--- mix.exs
+++ mix.exs
@@ -132,7 +132,7 @@ defmodule Teiserver.MixProject do
     [
       plt_core_path: "priv/plts",
       plt_file: {:no_warn, "priv/plts/dialyzer.plt"},
-      plt_add_apps: [:ex_unit]
+      plt_add_apps: [:ex_unit, :mix]
     ]
   end
 
