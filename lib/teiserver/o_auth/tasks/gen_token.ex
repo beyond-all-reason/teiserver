acc834ca241393a7ad1ed13c225625b3f52c7a74
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2024-07-28 18:22:17 +0100

    Add a mix task to generate an OAuth token
    Because doing the entire oauth flow without a client is a massive PITA,
    this should facilitate development.

---
+++ lib/teiserver/o_auth/tasks/gen_token.ex
@@ -0,0 +1,63 @@
+defmodule Teiserver.OAuth.Task.GenToken do
+  @moduledoc """
+  Generates a token for the given user. This is for a convenience task to ease
+  the development of anything requiring OAuth tokens like tachyon protocol.
+  """
+
+  alias Teiserver.OAuth.{ApplicationQueries, Token}
+
+  @spec create_token(String.t(), String.t() | nil) :: {:ok, Token.t()} | {:error, term()}
+  def create_token(username_or_email, app_uid \\ nil) do
+    with {:ok, user} <- get_user(username_or_email),
+         {:ok, app} <- get_app(app_uid) do
+      token_attr = %{
+        value: Base.hex_encode32(:crypto.strong_rand_bytes(32), padding: false),
+        owner_id: user.id,
+        application_id: app.id,
+        scopes: app.scopes,
+        expires_at: Timex.add(DateTime.utc_now(), Timex.Duration.from_days(1)),
+        type: :access,
+        refresh_token: nil
+      }
+
+      %Token{}
+      |> Token.changeset(token_attr)
+      |> Teiserver.Repo.insert()
+    else
+      {:error, msg} -> {:error, msg}
+    end
+  end
+
+  defp get_user(username_or_email) do
+    user =
+      if String.contains?(username_or_email, "@") do
+        Teiserver.Account.UserCacheLib.get_user_by_email(username_or_email)
+      else
+        Teiserver.Account.UserCacheLib.get_user_by_name(username_or_email)
+      end
+
+    if is_nil(user) do
+      {:error, "No user found for username or email #{username_or_email}"}
+    else
+      {:ok, user}
+    end
+  end
+
+  defp get_app(app_uid) do
+    apps =
+      ApplicationQueries.list_applications()
+      |> Enum.filter(fn app -> is_nil(app_uid) or app.uid == app_uid end)
+
+    case apps do
+      [app] ->
+        {:ok, app}
+
+      [] ->
+        {:error, "no oauth application found, need to create one first"}
+
+      apps ->
+        uids = Enum.map(apps, & &1.uid)
+        {:error, "more than one oauth application found #{inspect(uids)}"}
+    end
+  end
+end
