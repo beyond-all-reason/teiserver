287fd307b38d59044cc64c80b99c979aaf681ab3
bigbluejay9 <2730943+bigbluejay9@users.noreply.github.com>
2024-09-23 01:56:58 +0700

    Update SpringTcpServer state when joining channels (#479)
    Fixes issue #195.

--- lib/teiserver/protocols/spring/spring_in.ex
+++ lib/teiserver/protocols/spring/spring_in.ex
@@ -648,25 +648,26 @@ defmodule Teiserver.Protocols.SpringIn do
           :nomatch
       end
 
-    case regex_result do
-      :nomatch ->
-        _no_match(state, "JOIN", msg_id, data)
+    new_state =
+      case regex_result do
+        :nomatch ->
+          _no_match(state, "JOIN", msg_id, data)
 
-      {room_name, _key} ->
-        case Room.can_join_room?(state.userid, room_name) do
-          true ->
-            SpringOut.do_join_room(state, room_name)
+        {room_name, _key} ->
+          case Room.can_join_room?(state.userid, room_name) do
+            true ->
+              SpringOut.do_join_room(state, room_name)
 
-          {false, reason} ->
-            reply(:join_failure, {room_name, reason}, msg_id, state)
-        end
-    end
+            {false, reason} ->
+              reply(:join_failure, {room_name, reason}, msg_id, state)
+          end
+      end
 
-    if not state.exempt_from_cmd_throttle do
+    if not new_state.exempt_from_cmd_throttle do
       :timer.sleep(Application.get_env(:teiserver, Teiserver)[:spring_post_state_change_delay])
     end
 
-    state
+    new_state
   end
 
   defp do_handle("LEAVE", room_name, msg_id, state) do
