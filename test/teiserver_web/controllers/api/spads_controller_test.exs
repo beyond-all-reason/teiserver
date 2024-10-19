d455939d31366dd63a1b91abc9f250c97e848116
bigbluejay9 <2730943+bigbluejay9@users.noreply.github.com>
2024-10-19 13:55:26 +0800

    Add tests

--- test/teiserver_web/controllers/api/spads_controller_test.exs
+++ test/teiserver_web/controllers/api/spads_controller_test.exs
@@ -1,6 +1,7 @@
 defmodule TeiserverWeb.API.SpadsControllerTest do
   use TeiserverWeb.ConnCase, async: false
   alias Teiserver.Account
+  alias Teiserver.Config
   alias Teiserver.Lobby
   alias Teiserver.Game.MatchRatingLib
   alias Teiserver.Coordinator
@@ -14,15 +15,15 @@ defmodule TeiserverWeb.API.SpadsControllerTest do
       make_lobby: 1
     ]
 
-  defp make_rating(userid, rating_type_id, rating_value) do
+  defp make_rating(userid, rating_type_id, rating_value, opts \\ []) do
     {:ok, _} =
       Account.create_rating(%{
         user_id: userid,
         rating_type_id: rating_type_id,
         rating_value: rating_value,
-        skill: rating_value,
-        uncertainty: 0,
-        leaderboard_rating: rating_value,
+        skill: Keyword.get(opts, :skill, rating_value),
+        uncertainty: Keyword.get(opts, :uncertainty, 0),
+        leaderboard_rating: Keyword.get(opts, :leaderboard_rating, rating_value),
         last_updated: Timex.now()
       })
   end
@@ -63,6 +64,135 @@ defmodule TeiserverWeb.API.SpadsControllerTest do
       data = Jason.decode!(response)
 
       assert data == %{"rating_value" => 20, "uncertainty" => 5}
+    end
+
+    test "too much uncertainty", %{conn: conn} do
+      user = new_user()
+      rating_type_id = MatchRatingLib.rating_type_name_lookup()["Small Team"]
+      Config.update_site_config("teiserver.Uncertainty required to show rating", 10)
+
+      {:ok, _} =
+        Account.create_rating(%{
+          user_id: user.id,
+          rating_type_id: rating_type_id,
+          rating_value: 1,
+          skill: 1,
+          uncertainty: 99,
+          leaderboard_rating: 1,
+          last_updated: Timex.now()
+        })
+
+      Client.login(user, :spring, "127.0.0.1")
+
+      lobby_id =
+        make_lobby(%{name: "Test", founder_id: user.id, founder_name: user.name})
+
+      Client.join_battle(user.id, lobby_id, true)
+
+      conn = get(conn, Routes.ts_spads_path(conn, :get_rating, user.id, "Small Team"))
+      response = response(conn, 200)
+      data = Jason.decode!(response)
+      assert data == %{"rating_value" => 0, "uncertainty" => 99}
+    end
+
+    test "rating shown to host is rank by rating value", %{conn: conn} do
+      [user_1, user_2, user_3] = for _ <- 1..3, do: new_user()
+      rating_type_id = MatchRatingLib.rating_type_name_lookup()["Large Team"]
+
+      make_rating(user_1.id, rating_type_id, 10, leaderboard_rating: 30)
+      make_rating(user_2.id, rating_type_id, 20, leaderboard_rating: 20)
+      make_rating(user_3.id, rating_type_id, 30, leaderboard_rating: 10)
+
+      Client.login(user_1, :spring, "127.0.0.1")
+
+      lobby_id =
+        make_lobby(%{name: "Test", founder_id: user_1.id, founder_name: user_1.name})
+
+      Client.join_battle(user_1.id, lobby_id, true)
+      Client.login(user_3, :spring, "127.0.0.1")
+      Client.join_battle(user_3.id, lobby_id, true)
+
+      Config.update_site_config(
+        "teiserver.Rating shown to hosts",
+        "Rank by in game rating (1 is lowest)"
+      )
+
+      conn = get(conn, Routes.ts_spads_path(conn, :get_rating, user_1.id, "Large Team"))
+      response = response(conn, 200)
+      data = Jason.decode!(response)
+      assert data == %{"rating_value" => 1, "uncertainty" => 0}
+
+      conn = get(conn, Routes.ts_spads_path(conn, :get_rating, user_3.id, "Large Team"))
+      response = response(conn, 200)
+      data = Jason.decode!(response)
+      assert data == %{"rating_value" => 3, "uncertainty" => 0}
+
+      Config.update_site_config(
+        "teiserver.Rating shown to hosts",
+        "Rank by in game rating (1 is highest)"
+      )
+
+      conn = get(conn, Routes.ts_spads_path(conn, :get_rating, user_1.id, "Large Team"))
+      response = response(conn, 200)
+      data = Jason.decode!(response)
+      assert data == %{"rating_value" => 3, "uncertainty" => 0}
+
+      conn = get(conn, Routes.ts_spads_path(conn, :get_rating, user_3.id, "Large Team"))
+      response = response(conn, 200)
+      data = Jason.decode!(response)
+      assert data == %{"rating_value" => 1, "uncertainty" => 0}
+
+      Config.update_site_config("teiserver.Rating shown to hosts", "Rating value")
+    end
+
+    test "rating shown to host is rank by leaderboard rating", %{conn: conn} do
+      [user_1, user_2, user_3] = for _ <- 1..3, do: new_user()
+      rating_type_id = MatchRatingLib.rating_type_name_lookup()["Large Team"]
+
+      make_rating(user_1.id, rating_type_id, 30, leaderboard_rating: 10)
+      make_rating(user_2.id, rating_type_id, 20, leaderboard_rating: 20)
+      make_rating(user_3.id, rating_type_id, 10, leaderboard_rating: 30)
+
+      Client.login(user_1, :spring, "127.0.0.1")
+
+      lobby_id =
+        make_lobby(%{name: "Test", founder_id: user_1.id, founder_name: user_1.name})
+
+      Client.join_battle(user_1.id, lobby_id, true)
+      Client.login(user_3, :spring, "127.0.0.1")
+      Client.join_battle(user_3.id, lobby_id, true)
+
+      Config.update_site_config(
+        "teiserver.Rating shown to hosts",
+        "Rank by leaderboard rating (1 is lowest)"
+      )
+
+      conn = get(conn, Routes.ts_spads_path(conn, :get_rating, user_1.id, "Large Team"))
+      response = response(conn, 200)
+      data = Jason.decode!(response)
+      assert data == %{"rating_value" => 1, "uncertainty" => 0}
+
+      conn = get(conn, Routes.ts_spads_path(conn, :get_rating, user_3.id, "Large Team"))
+      response = response(conn, 200)
+      data = Jason.decode!(response)
+      assert data == %{"rating_value" => 3, "uncertainty" => 0}
+
+      Config.update_site_config(
+        "teiserver.Rating shown to hosts",
+        "Rank by leaderboard rating (1 is highest)"
+      )
+
+      conn = get(conn, Routes.ts_spads_path(conn, :get_rating, user_1.id, "Large Team"))
+      response = response(conn, 200)
+      data = Jason.decode!(response)
+      assert data == %{"rating_value" => 3, "uncertainty" => 0}
+
+      conn = get(conn, Routes.ts_spads_path(conn, :get_rating, user_3.id, "Large Team"))
+      response = response(conn, 200)
+      data = Jason.decode!(response)
+      assert data == %{"rating_value" => 1, "uncertainty" => 0}
+
+      Config.update_site_config("teiserver.Rating shown to hosts", "Rating value")
     end
   end
 
