1076f1022206187c0998d750f6f193da67689a0b
Spencer Collins <CollinsSpencer97@gmail.com>
2025-01-14 20:14:38 -0600

    fix oauth discovery in prod now

--- lib/teiserver_web/views/api/o_auth/code_view.ex
+++ lib/teiserver_web/views/api/o_auth/code_view.ex
@@ -22,7 +22,7 @@ defmodule TeiserverWeb.OAuth.CodeView do
   end
 
   def metadata(_) do
-    base = TeiserverWeb.Endpoint.static_url()
+    base = Application.get_env(:teiserver, Teiserver.OAuth)[:issuer] || TeiserverWeb.Endpoint.url()
 
     %{
       issuer: base,
