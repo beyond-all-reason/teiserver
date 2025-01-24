759ce1081fa2998b343d712da79bb9368241f109
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2025-01-24 07:41:57 +0000

    log more metadata
    Don't use `:all` because this logs way too much, things like file, line,
    erl_level and so on.
    Maybe later if we go structured log

--- config/config.exs
+++ config/config.exs
@@ -96,24 +96,26 @@ config :teiserver, Teiserver,
 
 config :logger, :default_handler, false
 
+metadata = [:request_id, :user_id, :queue_id]
+
 config :logger, LoggerBackends.Console,
   format: "$date $time [$level] $metadata $message\n",
-  metadata: [:request_id, :user_id],
+  metadata: metadata,
   level: :debug
 
 config :logger, :error_log,
   format: "$time [$level] $metadata $message\n",
-  metadata: [:request_id, :user_id],
+  metadata: metadata,
   level: :error
 
 config :logger, :notice_log,
   format: "$time [$level] $metadata $message\n",
-  metadata: [:request_id, :user_id],
+  metadata: metadata,
   level: :notice
 
 config :logger, :info_log,
   format: "$time [$level] $metadata $message\n",
-  metadata: [:request_id, :user_id],
+  metadata: metadata,
   level: :info
 
 # Use Jason for JSON parsing in Phoenix
