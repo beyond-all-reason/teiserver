0e78e1dd8645da2adcc730b55e63a8e9fb998ceb
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2025-02-15 18:18:27 +0000

    upgrade credo

--- mix.exs
+++ mix.exs
@@ -87,7 +87,7 @@ defmodule Teiserver.MixProject do
       {:phoenix_pubsub, "~> 2.0"},
       {:elixir_uuid, "~> 1.2"},
       {:excoveralls, "~> 0.15.3", only: :test, runtime: false},
-      {:credo, "~> 1.6", only: [:dev, :test], runtime: false},
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.1", only: [:dev, :test], runtime: false},
       {:dart_sass, "~> 0.6", only: [:dev]},
       {:libcluster, "~> 3.3"},
