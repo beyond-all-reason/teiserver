eaed781bb715235957f849ad200d894a2cfdfe8e
Lexon <44340857+L-e-x-o-n@users.noreply.github.com>
2025-02-19 23:02:07 +0100

    Add create_or_update_map to AssetFixtures
    Creates a new map if it doesn't exist, otherwise expands the existing map's matchmaking queue

--- test/support/fixtures/asset.ex
+++ test/support/fixtures/asset.ex
@@ -1,8 +1,28 @@
 defmodule Teiserver.AssetFixtures do
+  require Logger
   alias Teiserver.Asset
   alias Teiserver.Repo
 
   def create_map(attrs) do
     %Asset.Map{} |> Asset.Map.changeset(attrs) |> Repo.insert!()
+  end
+
+  def create_or_update_map(attrs) do
+    case Asset.get_map(attrs.spring_name) do
+      nil -> create_map(attrs)
+      map -> update_map(map, attrs)
+    end
+  end
+
+  def update_map(map, attrs) do
+    Logger.warning(
+      "Map #{map.spring_name} already exists, adding matchmaking queue #{inspect(attrs.matchmaking_queues)}"
+    )
+
+    map
+    |> Asset.Map.changeset(%{
+      matchmaking_queues: map.matchmaking_queues ++ attrs.matchmaking_queues
+    })
+    |> Repo.update!()
   end
 end
