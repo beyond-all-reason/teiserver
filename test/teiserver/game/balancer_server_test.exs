82f3eae2d475d020690d25cc0fd11f872171c863
David Norton <david.k.norton@gmail.com>
2025-03-14 20:22:32 -0500

    split sync and async tests, use Enum.zip

--- test/teiserver/game/balancer_server_test.exs
+++ test/teiserver/game/balancer_server_test.exs
@@ -1,54 +1,12 @@
-defmodule Teiserver.Game.BalancerServerTest do
+defmodule Teiserver.Game.BalancerServerAsyncTest do
   @moduledoc false
-  use Teiserver.DataCase, async: false
+  use ExUnit.Case, async: true
 
   @moduletag :balance_test
   alias Teiserver.Game.BalancerServer
 
-  def user_fixture(), do: make_user(%{"permissions" => []})
-
-  def make_user(params \\ %{}) do
-    requested_user = %{
-      "name" => params["name"] || "Test",
-      "email" => params["email"] || "email@email#{:rand.uniform(999_999_999_999)}",
-      "colour" => params["colour"] || "#00AA00",
-      "icon" => params["icon"] || "fa-solid fa-user",
-      "password" => params["password"] || "password",
-      "password_confirmation" => params["password"] || "password",
-      "data" => params["data"] || %{}
-    }
-
-    {:ok, u} =
-      Teiserver.Account.create_user(requested_user)
-  end
-
-  @spec make_users_with_ranks([non_neg_integer()]) ::
-          list()
-  def make_users_with_ranks(list_of_ranks_to_assign_to_users) do
-    users =
-      Enum.with_index(list_of_ranks_to_assign_to_users)
-      |> Enum.map(fn {user, index} ->
-        %{
-          "name" => "user" <> to_string(index + 1) <> "_" <> to_string(ExULID.ULID.generate()),
-          "email" => to_string(ExULID.ULID.generate()) <> "@example.com",
-          "permissions" => []
-        }
-      end)
-      |> Enum.map(fn x -> make_user(x) end)
-      # unwrap user creation response
-      |> Enum.map(fn {:ok, reply} -> reply end)
-      |> Enum.map(fn x -> Map.put(x, :userid, x.id) end)
-      # each user in their own party
-      |> Enum.map(fn x -> Map.put(x, :party_id, x.id + 1) end)
-
-    # set the user rank
-    Enum.map(Enum.with_index(users), fn {user, index} ->
-      Teiserver.Account.update_user_stat(user.id, %{
-        :rank => Enum.at(list_of_ranks_to_assign_to_users, index)
-      })
-    end)
-
-    users
+  defp create_fake_players(count) do
+    1..count |> Enum.map(fn _ -> %{} end)
   end
 
   test "get fuzz multiplier" do
@@ -81,6 +39,60 @@ defmodule Teiserver.Game.BalancerServerTest do
     players = create_fake_players(7)
     result = BalancerServer.calculate_team_size(team_count, players)
     assert result == 4
+  end
+end
+
+defmodule Teiserver.Game.BalancerServerTest do
+  @moduledoc false
+  use Teiserver.DataCase, async: false
+
+  @moduletag :balance_test
+  alias Teiserver.Game.BalancerServer
+
+  def user_fixture(), do: make_user(%{"permissions" => []})
+
+  def make_user(params \\ %{}) do
+    requested_user = %{
+      "name" => params["name"] || "Test",
+      "email" => params["email"] || "email@email#{:rand.uniform(999_999_999_999)}",
+      "colour" => params["colour"] || "#00AA00",
+      "icon" => params["icon"] || "fa-solid fa-user",
+      "password" => params["password"] || "password",
+      "password_confirmation" => params["password"] || "password",
+      "data" => params["data"] || %{}
+    }
+
+    {:ok, u} =
+      Teiserver.Account.create_user(requested_user)
+  end
+
+  @spec make_users_with_ranks([non_neg_integer()]) ::
+          list()
+  def make_users_with_ranks(list_of_ranks) do
+    users =
+      Enum.with_index(list_of_ranks)
+      |> Enum.map(fn {user, index} ->
+        %{
+          "name" => "user" <> to_string(index + 1) <> "_" <> to_string(ExULID.ULID.generate()),
+          "email" => to_string(ExULID.ULID.generate()) <> "@example.com",
+          "permissions" => []
+        }
+      end)
+      |> Enum.map(fn x -> make_user(x) end)
+      # unwrap user creation response
+      |> Enum.map(fn {:ok, reply} -> reply end)
+      |> Enum.map(fn x -> Map.put(x, :userid, x.id) end)
+      # each user in their own party
+      |> Enum.map(fn x -> Map.put(x, :party_id, x.id) end)
+
+    # set the user rank
+    for {user, rank} <- Enum.zip(users, list_of_ranks) do
+      Teiserver.Account.update_user_stat(user.id, %{
+        :rank => rank
+      })
+    end
+
+    users
   end
 
   test "load some players in and run a balance pass or two" do
@@ -177,9 +189,5 @@ defmodule Teiserver.Game.BalancerServerTest do
       GenServer.call(pid, :get_current_balance)
 
     refute get_balance_mode_returns_value == nil
-  end
-
-  defp create_fake_players(count) do
-    1..count |> Enum.map(fn _ -> %{} end)
   end
 end
