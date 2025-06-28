27f9bb6ac1b356b9616db10accbcf7636c51f039
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2025-06-28 08:34:28 +0100

    Refactor: move some logic in base algo module
    The logic to generate a stream of matches is going to get reused for
    other algo before filtering, so move it in the common location

--- lib/teiserver/matchmaking/algos.ex
+++ lib/teiserver/matchmaking/algos.ex
@@ -4,6 +4,7 @@ defmodule Teiserver.Matchmaking.Algos do
   """
 
   alias Teiserver.Matchmaking.Member
+  alias Teiserver.Helpers.Combi
 
   @type state :: term()
 
@@ -22,4 +23,62 @@ defmodule Teiserver.Matchmaking.Algos do
               members :: [Member.t()],
               state :: state()
             ) :: :no_match | {:match, [[[Member.t()]]]}
+
+  @spec match_members(
+          [Member.t()],
+          team_size :: pos_integer(),
+          team_count :: pos_integer(),
+          acc :: term()
+        ) :: [[[Member.t()]]]
+  def match_members(members, team_size, team_count, acc \\ []) do
+    res =
+      match_stream(members, team_size, team_count)
+      |> Enum.take(1)
+      |> List.first()
+
+    case res do
+      nil ->
+        acc
+
+      match ->
+        ids = for team <- match, member <- team, into: MapSet.new(), do: member.id
+
+        remaining_members =
+          Enum.filter(members, fn m ->
+            not MapSet.member?(ids, m.id)
+          end)
+
+        match_members(remaining_members, team_size, team_count, [match | acc])
+    end
+  end
+
+  # Returns an enumerable of matches for the given members, team size and team count
+  # a match is a list of `team_count` teams. Each teams is a list of member such
+  # that the number of player in the team is `team_size`
+  @spec match_stream(
+          members :: [Member.t()],
+          team_size :: pos_integer(),
+          team_count :: pos_integer()
+        ) :: Enumerable.t([[[Member.t()]]])
+  def match_stream(members, team_size, team_count) when team_count <= 1 do
+    Combi.combinations(members, team_size)
+    |> Stream.map(fn t -> [t] end)
+  end
+
+  def match_stream(members, team_size, team_count) do
+    teams = Combi.combinations(members, team_size)
+
+    Stream.flat_map(teams, fn team ->
+      ids = for member <- team, into: MapSet.new(), do: member.id
+
+      available_members =
+        Enum.filter(members, fn m ->
+          not MapSet.member?(ids, m.id)
+        end)
+
+      other_matches = match_stream(available_members, team_size, team_count - 1)
+
+      Stream.map(other_matches, fn teams -> [team | teams] end)
+    end)
+  end
 end
