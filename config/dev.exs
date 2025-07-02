4e3d24ac608255fc02228d0f9193fdd35e84e36b
JayLabel <jlabcissc@gmail.com>
2025-07-02 10:07:51 -0400

    update dev.exe for docker

--- config/dev.exs
+++ config/dev.exs
@@ -4,13 +4,23 @@ config :teiserver, Teiserver.Setup, key: "dev_key"
 
 # Configure your database
 config :teiserver, Teiserver.Repo,
-  username: "teiserver_dev",
-  password: "123456789",
-  database: "teiserver_dev",
-  hostname: "localhost",
+  username: System.get_env("TEI_DB_USERNAME") || "teiserver_dev",
+  password: System.get_env("TEI_DB_PASSWORD") || "123456789",
+  database: System.get_env("TEI_DB_NAME") || "teiserver_dev",
+  hostname: System.get_env("TEI_DB_HOSTNAME") || "localhost",
+  port:     String.to_integer(System.get_env("TEI_DB_PORT") || "5432"),
   show_sensitive_data_on_connection_error: true,
   pool_size: 10,
   timeout: 180_000
+
+
+# Helper function to set the bind_ip for the container
+# Fallback to the original value if not set
+bind_ip =
+  (System.get_env("TEI_BIND_IP") || "127.0.0.1")
+  |> String.split(".")
+  |> Enum.map(&String.to_integer/1)
+  |> List.to_tuple()
 
 # For development, we disable any cache and enable
 # debugging and code reloading.
