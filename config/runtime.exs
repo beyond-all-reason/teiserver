ee41ce81d0dfdd8c1441f5d92de2b74a77132889
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2025-09-12 16:49:22 +0100

    Remove prod-specific configuration
    This way, one can run a full release locally, without having to do the
    whole TLS and mailer setup.

--- config/runtime.exs
+++ config/runtime.exs
@@ -27,113 +27,182 @@ end
 # used for mailing, checking origins, finding tls certs…
 domain_name = Teiserver.ConfigHelpers.get_env("TEI_DOMAIN_NAME", "beyondallreason.info")
 
-# Only do some runtime configuration in production since in dev and tests the
-# files are automatically recompiled on the fly and thus, config/{dev,test}.exs
-# are just fine
-if config_env() == :prod do
+# this is used in lib/teiserver_web/controllers/account/setup_controller.ex
+# as a special endpoint to create the root user. Setting it to empty or nil
+# will disable the functionality completely.
+config :teiserver, Teiserver.Setup,
+  key: Teiserver.ConfigHelpers.get_env("TEI_SETUP_ROOT_KEY", nil)
+
+config :teiserver,
+  blog_allow_upload: System.get_env("TEI_BLOG_ALLOW_UPLOAD") == "TRUE",
+  blog_upload_path: System.get_env("TEI_BLOG_UPLOAD_PATH"),
+  blog_upload_extensions:
+    System.get_env("TEI_BLOG_UPLOAD_EXTENSIONS", ".jpg .jpeg .png") |> String.split(" ")
+
+config :teiserver, Teiserver,
+  game_name: "Beyond All Reason",
+  game_name_short: "BAR",
+  main_website: "https://www.beyondallreason.info/",
+  privacy_email: "privacy@beyondallreason.info",
+  website: [
+    url: "beyondallreason.info"
+  ],
+  server_flag: "GB-WLS",
+  enable_benchmark: false,
+  node_name: Teiserver.ConfigHelpers.get_env("TEI_NODE_NAME", "local"),
+  enable_managed_lobbies: true,
+  user_agreement:
+    "A verification code has been sent to your email address. Please read our terms of service at https://#{domain_name}/privacy_policy and the code of conduct at https://www.beyondallreason.info/code-of-conduct. Then enter your six digit code below if you agree to the terms."
+
+config :teiserver, Teiserver.Repo,
+  hostname: Teiserver.ConfigHelpers.get_env("TEI_DB_HOSTNAME", "localhost"),
+  username: Teiserver.ConfigHelpers.get_env("TEI_DB_USERNAME", "teiserver_dev"),
+  password: Teiserver.ConfigHelpers.get_env("TEI_DB_PASSWORD", "123456789"),
+  database: Teiserver.ConfigHelpers.get_env("TEI_DB_NAME", "teiserver_dev"),
+  pool_size: 40,
+  timeout: 120_000,
+  queue_interval: 2000
+
+check_origin =
+  if Teiserver.ConfigHelpers.get_env("TEI_SHOULD_CHECK_ORIGIN", false, :bool) do
+    ["//#{domain_name}", "//*.#{domain_name}"]
+  else
+    false
+  end
+
+config :teiserver, TeiserverWeb.Endpoint,
+  url: [host: domain_name],
+  check_origin: check_origin,
+  http: [:inet6, port: Teiserver.ConfigHelpers.get_env("TEI_PORT", "4000", :int)],
+  # the default is meant for ease of local dev
+  secret_key_base:
+    Teiserver.ConfigHelpers.get_env(
+      "TEI_HTTP_SECRET_KEY_BASE",
+      "6FN12Jv4ZITAK1fq7ehD0MTRvbLsXYWj+wLY3ifkzzlcUIcpUJK7aG/ptrJSemAy"
+    )
+
+use_tls? = Teiserver.ConfigHelpers.get_env("TEI_TLS_PRIVATE_KEY_PATH", nil) != nil
+config :teiserver, Teiserver, use_tls?: use_tls?
+
+if use_tls? do
   certificates = [
     keyfile: Teiserver.ConfigHelpers.get_env("TEI_TLS_PRIVATE_KEY_PATH"),
     certfile: Teiserver.ConfigHelpers.get_env("TEI_TLS_CERT_PATH"),
     cacertfile: Teiserver.ConfigHelpers.get_env("TEI_TLS_CA_CERT_PATH")
   ]
 
-  # this is used in lib/teiserver_web/controllers/account/setup_controller.ex
-  # as a special endpoint to create the root user. Setting it to empty or nil
-  # will disable the functionality completely.
-  config :teiserver, Teiserver.Setup,
-    key: Teiserver.ConfigHelpers.get_env("TEI_SETUP_ROOT_KEY", nil)
-
-  config :teiserver,
-    blog_allow_upload: System.get_env("TEI_BLOG_ALLOW_UPLOAD") == "TRUE",
-    blog_upload_path: System.get_env("TEI_BLOG_UPLOAD_PATH"),
-    blog_upload_extensions:
-      System.get_env("TEI_BLOG_UPLOAD_EXTENSIONS", ".jpg .jpeg .png") |> String.split(" ")
-
   config :teiserver, Teiserver,
-    game_name: "Beyond All Reason",
-    game_name_short: "BAR",
-    main_website: "https://www.beyondallreason.info/",
-    privacy_email: "privacy@beyondallreason.info",
     ports: [
       tcp: Teiserver.ConfigHelpers.get_env("TEI_SPRING_TCP_PORT", 8200, :int),
       tls: Teiserver.ConfigHelpers.get_env("TEI_SPRING_TLS_PORT", 8201, :int)
     ],
-    certs: certificates,
-    website: [
-      url: "beyondallreason.info"
-    ],
-    server_flag: "GB-WLS",
-    enable_benchmark: false,
-    node_name: Teiserver.ConfigHelpers.get_env("TEI_NODE_NAME"),
-    enable_managed_lobbies: true,
-    user_agreement:
-      "A verification code has been sent to your email address. Please read our terms of service at https://#{domain_name}/privacy_policy and the code of conduct at https://www.beyondallreason.info/code-of-conduct. Then enter your six digit code below if you agree to the terms."
-
-  config :teiserver, Teiserver.Repo,
-    hostname: Teiserver.ConfigHelpers.get_env("TEI_DB_HOSTNAME"),
-    username: Teiserver.ConfigHelpers.get_env("TEI_DB_USERNAME"),
-    password: Teiserver.ConfigHelpers.get_env("TEI_DB_PASSWORD"),
-    database: Teiserver.ConfigHelpers.get_env("TEI_DB_NAME"),
-    pool_size: 40,
-    timeout: 120_000,
-    queue_interval: 2000
-
-  check_origin =
-    if Teiserver.ConfigHelpers.get_env("TEI_SHOULD_CHECK_ORIGIN", false, :bool) do
-      ["//#{domain_name}", "//*.#{domain_name}"]
-    else
-      false
-    end
+    certs: certificates
 
   config :teiserver, TeiserverWeb.Endpoint,
-    url: [host: domain_name],
-    check_origin: check_origin,
     https:
       certificates ++
         [
           versions: [:"tlsv1.2"],
           # dhfile is not supported for tls 1.3
           # https://www.erlang.org/doc/man/ssl.html#type-dh_file
-          dhfile: Teiserver.ConfigHelpers.get_env("TEI_TLS_DH_FILE_PATH", "/etc/ssl/dhparam.pem")
+          dhfile: Teiserver.ConfigHelpers.get_env("TEI_TLS_DH_FILE_PATH", "/etc/ssl/dhparam.pem"),
+          port: 8888,
+          otp_app: :teiserver,
+          ciphers: [
+            ~c"ECDHE-ECDSA-AES256-GCM-SHA384",
+            ~c"ECDHE-RSA-AES256-GCM-SHA384",
+            ~c"ECDHE-ECDSA-AES256-SHA384",
+            ~c"ECDHE-RSA-AES256-SHA384",
+            ~c"ECDHE-ECDSA-DES-CBC3-SHA",
+            ~c"ECDH-ECDSA-AES256-GCM-SHA384",
+            ~c"ECDH-RSA-AES256-GCM-SHA384",
+            ~c"ECDH-ECDSA-AES256-SHA384",
+            ~c"ECDH-RSA-AES256-SHA384",
+            ~c"DHE-DSS-AES256-GCM-SHA384",
+            ~c"DHE-DSS-AES256-SHA256",
+            ~c"AES256-GCM-SHA384",
+            ~c"AES256-SHA256",
+            ~c"ECDHE-ECDSA-AES128-GCM-SHA256",
+            ~c"ECDHE-RSA-AES128-GCM-SHA256",
+            ~c"ECDHE-ECDSA-AES128-SHA256",
+            ~c"ECDHE-RSA-AES128-SHA256",
+            ~c"ECDH-ECDSA-AES128-GCM-SHA256",
+            ~c"ECDH-RSA-AES128-GCM-SHA256",
+            ~c"ECDH-ECDSA-AES128-SHA256",
+            ~c"ECDH-RSA-AES128-SHA256",
+            ~c"DHE-DSS-AES128-GCM-SHA256",
+            ~c"DHE-DSS-AES128-SHA256",
+            ~c"AES128-GCM-SHA256",
+            ~c"AES128-SHA256",
+            ~c"ECDHE-ECDSA-AES256-SHA",
+            ~c"ECDHE-RSA-AES256-SHA",
+            ~c"DHE-DSS-AES256-SHA",
+            ~c"ECDH-ECDSA-AES256-SHA",
+            ~c"ECDH-RSA-AES256-SHA",
+            ~c"AES256-SHA",
+            ~c"ECDHE-ECDSA-AES128-SHA",
+            ~c"ECDHE-RSA-AES128-SHA",
+            ~c"DHE-DSS-AES128-SHA",
+            ~c"ECDH-ECDSA-AES128-SHA",
+            ~c"ECDH-RSA-AES128-SHA",
+            ~c"AES128-SHA"
+          ],
+          secure_renegotiate: true,
+          reuse_sessions: true,
+          honor_cipher_order: true
         ],
-    http: [:inet6, port: Teiserver.ConfigHelpers.get_env("TEI_PORT", "4000", :int)],
-    secret_key_base: Teiserver.ConfigHelpers.get_env("TEI_HTTP_SECRET_KEY_BASE")
+    force_ssl: [hsts: true],
+    root: ".",
+    cache_static_manifest: "priv/static/cache_manifest.json",
+    server: true,
+    version: Mix.Project.config()[:version]
+else
+  config :teiserver, Teiserver,
+    ports: [
+      tcp: Teiserver.ConfigHelpers.get_env("TEI_SPRING_TCP_PORT", 8200, :int)
+    ]
+end
 
-  config :teiserver, Teiserver.Account.Guardian,
-    issuer: Teiserver.ConfigHelpers.get_env("TEI_GUARDIAN_ISSUER", "teiserver"),
-    secret_key: Teiserver.ConfigHelpers.get_env("TEI_GUARDIAN_SECRET_KEY")
+config :teiserver, Teiserver.Account.Guardian,
+  issuer: Teiserver.ConfigHelpers.get_env("TEI_GUARDIAN_ISSUER", "teiserver"),
+  # the default is only there so we can run it easily
+  secret_key:
+    Teiserver.ConfigHelpers.get_env(
+      "TEI_GUARDIAN_SECRET_KEY",
+      "9vJcJOYwsjdIQ9IhfOI5F9GQMykuNjBW58FY9S/TqMsq6gRdKgY05jscQAFVKfwa"
+    ),
+  ttl: {30, :days}
 
-  config :teiserver, Teiserver.OAuth, issuer: "https://#{domain_name}"
+config :teiserver, Teiserver.OAuth, issuer: "https://#{domain_name}"
 
-  if Teiserver.ConfigHelpers.get_env("TEI_ENABLE_EMAIL_INTEGRATION", true, :bool) do
-    config :teiserver, Teiserver.Mailer,
-      adapter: Bamboo.SMTPAdapter,
-      contact_address:
-        Teiserver.ConfigHelpers.get_env("TEI_CONTACT_EMAIL_ADDRESS", "info@#{domain_name}"),
-      noreply_name: "Beyond All Reason",
-      noreply_address:
-        Teiserver.ConfigHelpers.get_env("TEI_NOREPLY_EMAIL_ADDRESS", "noreply@#{domain_name}"),
-      server: Teiserver.ConfigHelpers.get_env("TEI_SMTP_SERVER"),
-      hostname: Teiserver.ConfigHelpers.get_env("TEI_SMTP_HOSTNAME"),
-      # port: 1025,
-      port: Teiserver.ConfigHelpers.get_env("TEI_SMTP_PORT", "587", :int),
-      username: Teiserver.ConfigHelpers.get_env("TEI_SMTP_USERNAME"),
-      password: Teiserver.ConfigHelpers.get_env("TEI_SMTP_PASSWORD"),
-      # tls: :if_available, # can be `:always` or `:never`
-      # can be `:always` or `:never`
-      tls: :always,
-      tls_verify:
-        if(Teiserver.ConfigHelpers.get_env("TEI_SMTP_TLS_VERIFY", true, :bool),
-          do: :verify_peer,
-          else: :verify_none
-        ),
-      # or {":system", ALLOWED_TLS_VERSIONS"} w/ comma seprated values (e.g. "tlsv1.1,tlsv1.2")
-      allowed_tls_versions: [:"tlsv1.2"],
-      # can be `true`
-      no_mx_lookups: false,
-      # auth: :if_available # can be `always`. If your smtp relay requires authentication set it to `always`.
-      auth: :always
-  end
+if Teiserver.ConfigHelpers.get_env("TEI_ENABLE_EMAIL_INTEGRATION", false, :bool) do
+  config :teiserver, Teiserver.Mailer,
+    adapter: Bamboo.SMTPAdapter,
+    contact_address:
+      Teiserver.ConfigHelpers.get_env("TEI_CONTACT_EMAIL_ADDRESS", "info@#{domain_name}"),
+    noreply_name: "Beyond All Reason",
+    noreply_address:
+      Teiserver.ConfigHelpers.get_env("TEI_NOREPLY_EMAIL_ADDRESS", "noreply@#{domain_name}"),
+    server: Teiserver.ConfigHelpers.get_env("TEI_SMTP_SERVER"),
+    hostname: Teiserver.ConfigHelpers.get_env("TEI_SMTP_HOSTNAME"),
+    # port: 1025,
+    port: Teiserver.ConfigHelpers.get_env("TEI_SMTP_PORT", "587", :int),
+    username: Teiserver.ConfigHelpers.get_env("TEI_SMTP_USERNAME"),
+    password: Teiserver.ConfigHelpers.get_env("TEI_SMTP_PASSWORD"),
+    # tls: :if_available, # can be `:always` or `:never`
+    # can be `:always` or `:never`
+    tls: :always,
+    tls_verify:
+      if(Teiserver.ConfigHelpers.get_env("TEI_SMTP_TLS_VERIFY", true, :bool),
+        do: :verify_peer,
+        else: :verify_none
+      ),
+    # or {":system", ALLOWED_TLS_VERSIONS"} w/ comma seprated values (e.g. "tlsv1.1,tlsv1.2")
+    allowed_tls_versions: [:"tlsv1.2"],
+    # can be `true`
+    no_mx_lookups: false,
+    # auth: :if_available # can be `always`. If your smtp relay requires authentication set it to `always`.
+    auth: :always
 end
 
 log_root_path = Teiserver.ConfigHelpers.get_env("TEI_LOG_ROOT_PATH", "/tmp/teiserver")
