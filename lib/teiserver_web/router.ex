4d9e1f73945db9631bee5a2b3d0d55f6d98597ad
Aurora <git@chocobos.de>
2025-09-25 20:15:53 +0200

    current updates. Nothing working yet

--- lib/teiserver_web/router.ex
+++ lib/teiserver_web/router.ex
@@ -281,6 +281,7 @@ defmodule TeiserverWeb.Router do
     put("/update_details", GeneralController, :update_details)
 
     get("/security", SecurityController, :index)
+    live "/security/totp", SecurityLive.TOTP, :totp
     get("/security/edit_password", SecurityController, :edit_password)
     put("/security/update_password", SecurityController, :update_password)
     delete("/security/delete_token/:id", SecurityController, :delete_token)
