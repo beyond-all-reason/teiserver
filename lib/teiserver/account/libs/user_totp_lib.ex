e219327fb12d6c758a44690082501af477d131d0
Aurora <git@chocobos.de>
2025-09-27 12:35:05 +0200

    cleaned up totp lib

--- lib/teiserver/account/libs/user_totp_lib.ex
+++ lib/teiserver/account/libs/user_totp_lib.ex
@@ -2,6 +2,108 @@ defmodule Teiserver.Account.TOTPLib do
   use TeiserverWeb, :library
   alias Teiserver.Account.{User, TOTP}
 
+  @spec get_user_totp(User.t()) :: {:active, TOTP.t()} | {:inactive, nil}
+  defp get_user_totp(%User{id: user_id}) do
+    case Repo.get_by(TOTP, user_id: user_id) do
+      nil ->
+        {:inactive, nil}
+
+      totp ->
+        {:ok, decoded_secret} = Base.decode32(totp.secret)
+        totp = %{totp | secret: decoded_secret}
+        {:active, totp}
+    end
+  end
+
+  @spec get_user_totp_status(User.t()) :: :active | :inactive
+  def get_user_totp_status(%User{} = user) do
+    {status, _totp} = get_user_totp(user)
+    status
+  end
+
+  @spec get_or_generate_secret(User.t()) :: {:new | :existing, binary()}
+  def get_or_generate_secret(%User{id: _user_id} = user) do
+    case get_user_totp(user) do
+      {:inactive, nil} ->
+        {:new, NimbleTOTP.secret()}
+
+      {:active, totp} ->
+        {:existing, totp.secret}
+    end
+  end
+
+  @spec get_user_secret(User.t()) :: binary | nil
+  def get_user_secret(%User{id: _user_id} = user) do
+    case get_user_totp(user) do
+      {:inactive, nil} ->
+        :inactive
+
+      {:active, totp} ->
+        totp.secret
+    end
+  end
+
+  @spec get_last_used_otp(User.t()) :: any
+  def get_last_used_otp(%User{id: _user_id} = user) do
+    case get_user_totp(user) do
+      {:inactive, nil} ->
+        :inactive
+
+      {:active, totp} ->
+        totp.last_used
+    end
+  end
+
+  @spec set_totp(User.t() | TOTP.t(), map()) ::
+          {:ok, TOTP.t()} | {:error, Ecto.Changeset.t()}
+  defp set_totp(%User{} = user, attrs) do
+    case get_user_totp(user) do
+      {:inactive, nil} ->
+        attrs =
+          Map.update(attrs, :secret, nil, fn secret ->
+            Base.encode32(secret, padding: false)
+          end)
+
+        TOTP.changeset(%TOTP{}, attrs)
+        |> Repo.insert()
+
+      {:active, totp} ->
+        set_totp(totp, attrs)
+    end
+  end
+
+  defp set_totp(%TOTP{} = totp, attrs) do
+    attrs =
+      Map.update(attrs, :secret, nil, fn secret ->
+        Base.encode32(secret, padding: false)
+      end)
+
+    totp
+    |> TOTP.changeset(attrs)
+    |> Repo.update()
+  end
+
+  @spec set_secret(User.t(), binary) :: {:ok, TOTP.t()} | {:error, Ecto.Changeset.t()}
+  def set_secret(%User{} = user, secret) do
+    set_totp(user, %{user_id: user.id, secret: secret})
+  end
+
+  @spec set_last_used(User.t(), String.t()) :: {:ok, TOTP.t()} | {:error, Ecto.Changeset.t()}
+  defp set_last_used(%User{} = user, last_used) do
+    set_totp(user, %{user_id: user.id, last_used: last_used})
+  end
+
+  @spec disable_totp(User.t()) :: {:ok, TOTP.t() | nil} | {:error, Ecto.Changeset.t()}
+  def disable_totp(%User{id: _user_id} = user) do
+    case get_user_totp(user) do
+      {:active, totp} ->
+        Repo.delete(totp)
+
+      {:inactive, _} ->
+        {:ok, nil}
+    end
+  end
+
   @spec validate_totp(User.t() | binary, String.t()) ::
           {:ok, :valid | :grace} | {:error, :inactive | :invalid | :used}
   def validate_totp(%User{id: _user_id} = user, otp) do
@@ -10,14 +112,12 @@ defmodule Teiserver.Account.TOTPLib do
         {:error, :inactive}
 
       {:active, totp} ->
-        if validate_last_used(totp, otp) do
+        if validate_last_used(totp.last_used, otp) do
           {:error, :used}
         else
-          # Validates OTP against the secret
           case validate_totp(totp.secret, otp) do
-            # Update last_used if OTP tested successful
             {:ok, info} ->
-              set_totp(totp, %{last_used: otp})
+              set_last_used(user, otp)
               {:ok, info}
 
             {:error, :invalid} ->
@@ -42,77 +142,8 @@ defmodule Teiserver.Account.TOTPLib do
     end
   end
 
-  @spec validate_last_used(TOTP.t(), String.t()) :: boolean
-  def validate_last_used(%TOTP{last_used: last_used}, otp) do
+  @spec validate_last_used(String.t(), String.t()) :: boolean
+  def validate_last_used(last_used, otp) do
     last_used == otp
-  end
-
-  #  Todo:
-  #  - Function that either gets the secret from the user, or generates a new secret and returns it (get_or_generate_secret) [Done]
-  #  - Function to generate a new secret for the user (reset_secret) [done]
-  #  - Function to set the secret for a user, if none exists (possibly_activate_2FA) [done]
-  #  - Function to delete the whole TOTP row for a user (deactivate_2FA) [done]
-
-  @spec get_or_generate_secret(User.t()) :: {:new | :existing, binary()}
-  def get_or_generate_secret(%User{id: _user_id} = user) do
-    case get_user_totp(user) do
-      {:inactive, nil} ->
-        {:new, NimbleTOTP.secret()}
-
-      {:active, totp} ->
-        {:existing, totp.secret}
-    end
-  end
-
-  @spec reset_secret(User.t()) :: {:ok, TOTP.t()} | {:error, Ecto.Changeset.t()}
-  def reset_secret(%User{} = user) do
-    secret = NimbleTOTP.secret()
-    set_totp(user, %{secret: secret})
-  end
-
-  @spec set_secret(User.t(), String.t()) :: {:ok, TOTP.t()} | {:error, Ecto.Changeset.t()}
-  def set_secret(%User{} = user, secret) do
-    set_totp(user, %{user_id: user.id, secret: secret})
-  end
-
-  @spec disable_totp(User.t()) :: {:ok, TOTP.t() | nil} | {:error, Ecto.Changeset.t()}
-  def disable_totp(%User{id: _user_id} = user) do
-    case get_user_totp(user) do
-      {:active, totp} ->
-        Repo.delete(totp)
-
-      {:inactive, _} ->
-        {:ok, nil}
-    end
-  end
-
-  @spec get_user_totp(User.t()) :: {:active, TOTP.t()} | {:inactive, nil}
-  def get_user_totp(%User{id: user_id}) do
-    case Repo.get_by(TOTP, user_id: user_id) do
-      nil ->
-        {:inactive, nil}
-
-      totp ->
-        {:active, totp}
-    end
-  end
-
-  @spec set_totp(User.t() | TOTP.t(), map()) ::
-          {:ok, TOTP.t()} | {:error, Ecto.Changeset.t()}
-  def set_totp(%User{} = user, attrs) do
-    case get_user_totp(user) do
-      {:inactive, nil} ->
-        TOTP.changeset(%TOTP{}, attrs)
-        |> Repo.insert()
-
-      {:active, totp} ->
-        set_totp(totp, attrs)
-    end
-  end
-
-  def set_totp(%TOTP{} = totp, attrs) do
-    totp
-    |> TOTP.changeset(attrs)
-    |> Repo.update()
   end
 end
