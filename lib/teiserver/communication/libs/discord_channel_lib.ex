c1b96ad55175b7b26c6ca8a162f2d8810ae50020
Aurora <git@chocobos.de>
2025-10-06 17:09:23 +0200

    fixed one issue of the bridge failing

--- lib/teiserver/communication/libs/discord_channel_lib.ex
+++ lib/teiserver/communication/libs/discord_channel_lib.ex
@@ -225,8 +225,15 @@ defmodule Teiserver.Communication.DiscordChannelLib do
       when is_integer(message_id) do
     if use_discord?() do
       case get_channel_id_from_any(maybe_channel_id) do
-        nil -> {:error, "No channel found"}
-        channel_id -> Nostrum.Api.Message.edit(channel_id, message_id, content: new_message)
+        nil ->
+          {:error, "No channel found"}
+
+        channel_id ->
+          try do
+            Nostrum.Api.Message.edit(channel_id, message_id, content: new_message)
+          rescue
+            e -> {:error, e}
+          end
       end
     else
       {:error, :discord_disabled}
@@ -280,7 +287,7 @@ defmodule Teiserver.Communication.DiscordChannelLib do
     if use_discord?() do
       case get_channel_id_from_any(maybe_channel_id) do
         nil -> {:error, "No channel found"}
-        channel_id -> Nostrum.Api.Message.react(channel_id, message_id, icon)
+        channel_id -> Nostrum.Api.Message.react(channel_id, message_id, URI.encode(icon))
       end
     else
       {:error, :discord_disabled}
@@ -293,7 +300,7 @@ defmodule Teiserver.Communication.DiscordChannelLib do
     if use_discord?() do
       case get_channel_id_from_any(maybe_channel_id) do
         nil -> {:error, "No channel found"}
-        channel_id -> Nostrum.Api.Message.unreact(channel_id, message_id, emoji)
+        channel_id -> Nostrum.Api.Message.unreact(channel_id, message_id, URI.encode(emoji))
       end
     else
       {:error, :discord_disabled}
