effa768e01e03d3a1b3889d00c5e0f546b07b118
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2025-12-23 17:23:28 +0100

    Switch to regular Registry for parties
    `gen_statem` does name resolution differently from `gen_server`.
    The process must exist in the registry used with the via_tuple upon entering
    the init function. But with Horde calling `register` followed
    by `lookup` isn't guaranteed to return a result, and in this case
    the start_link will fail.
    So revert to the regular registry to sidestep the problem. If/when we go
    multi node, we'll need to figure something out.
    Retrying a few times when this happen seems to work, so that may be good
    enough.

--- lib/teiserver/party/registry.ex
+++ lib/teiserver/party/registry.ex
@@ -7,32 +7,27 @@ defmodule Teiserver.Party.Registry do
 
   @spec lookup(Party.Server.id()) :: pid() | nil
   def lookup(party_id) do
-    case Horde.Registry.lookup(__MODULE__, party_id) do
+    case Registry.lookup(__MODULE__, party_id) do
       [{pid, _}] -> pid
       _ -> nil
     end
   end
 
   @spec count() :: non_neg_integer()
-  def count() do
-    case Horde.Registry.count(__MODULE__) do
-      :undefined -> 0
-      x -> x
-    end
-  end
+  def count(), do: Registry.count(__MODULE__)
 
   def start_link() do
-    Horde.Registry.start_link(keys: :unique, name: __MODULE__)
+    Registry.start_link(keys: :unique, name: __MODULE__)
   end
 
   def child_spec(_) do
-    Supervisor.child_spec(Horde.Registry,
+    Supervisor.child_spec(Registry,
       id: __MODULE__,
       start: {__MODULE__, :start_link, []}
     )
   end
 
   def via_tuple(party_id) do
-    {:via, Horde.Registry, {__MODULE__, party_id}}
+    {:via, Registry, {__MODULE__, party_id}}
   end
 end
