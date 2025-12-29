ae4772d1b49efc580fbc84dd4ff7b80223b564f7
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2025-12-29 17:49:49 +0000

    Session rejoin current party at startup.
    This doesn't yet handle parties the session was invited to.

--- test/teiserver_web/tachyon/party_test.exs
+++ test/teiserver_web/tachyon/party_test.exs
@@ -446,6 +446,30 @@ defmodule TeiserverWeb.Tachyon.PartyTest do
     end
   end
 
+  describe "party restoration" do
+    setup [{Tachyon, :setup_client}]
+
+    test "parties stays across restart", %{client: client, token: token} do
+      Teiserver.Tachyon.enable_state_restoration()
+      assert %{"status" => "success", "data" => data} = Tachyon.create_party!(client)
+      %{"partyId" => party_id} = data
+
+      Teiserver.Tachyon.restart_system()
+      assert {:error, :disconnected} == Tachyon.recv_message(client)
+
+      client = Tachyon.connect(token, swallow_first_event: false)
+
+      {:ok, %{"commandId" => "user/self", "data" => data}} = Tachyon.recv_message(client)
+      assert get_in(data, ["user", "party", "id"]) == party_id
+
+      # make sure the session correctly monitors the session
+      Teiserver.Party.lookup(party_id)
+      |> Process.exit(:kill)
+
+      %{"commandId" => "party/removed"} = Tachyon.recv_message!(client)
+    end
+  end
+
   defp setup_party(%{client: client}), do: setup_party(client)
 
   defp setup_party(client) do
