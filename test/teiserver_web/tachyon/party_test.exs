439ba5412282581b7584e7ae72a98ec61318f9bd
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2026-01-01 18:13:13 +0000

    Also rejoin party invited to

--- test/teiserver_web/tachyon/party_test.exs
+++ test/teiserver_web/tachyon/party_test.exs
@@ -468,6 +468,30 @@ defmodule TeiserverWeb.Tachyon.PartyTest do
 
       %{"commandId" => "party/removed"} = Tachyon.recv_message!(client)
     end
+
+    test "parties invites stays across restart", %{client: client} do
+      Teiserver.Tachyon.enable_state_restoration()
+      assert %{"status" => "success", "data" => data} = Tachyon.create_party!(client)
+      %{"partyId" => party_id} = data
+      ctx2 = setup_client()
+
+      assert %{"status" => "success"} = Tachyon.invite_to_party!(client, ctx2.user.id)
+      assert %{"commandId" => "party/invited"} = Tachyon.recv_message!(ctx2.client)
+
+      Teiserver.Tachyon.restart_system()
+      assert {:error, :disconnected} == Tachyon.recv_message(ctx2.client)
+
+      client = Tachyon.connect(ctx2.token, swallow_first_event: false)
+
+      {:ok, %{"commandId" => "user/self", "data" => data}} = Tachyon.recv_message(client)
+      assert [%{"id" => ^party_id}] = data["user"]["invitedToParties"]
+
+      # make sure the session correctly monitors the session
+      Teiserver.Party.lookup(party_id)
+      |> Process.exit(:kill)
+
+      %{"commandId" => "party/removed"} = Tachyon.recv_message!(client)
+    end
   end
 
   defp setup_party(%{client: client}), do: setup_party(client)
