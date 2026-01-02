27c831f51ccb6be65e52092b8ec8cc976b1d4463
Dany Marcoux <git@dmarcoux.com>
2026-01-02 20:19:06 +0100

    Fix Dialyzer warning in lib/teiserver.ex

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -1,5 +1,4 @@
 [
-  {"lib/teiserver.ex", :invalid_contract},
   {"lib/teiserver/account.ex", :unknown_type},
   {"lib/teiserver/account.ex", :invalid_contract},
   # This started after inverting a condition in an `if/else` statement. It's hard to fix with all the other ignored warnings here.
