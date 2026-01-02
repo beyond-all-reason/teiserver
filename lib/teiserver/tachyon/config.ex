28bcdfc5442bb0daaa8d41df497e5fa8879f8886
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2026-01-02 08:14:27 +0000

    Timeout party when session take too long to rejoin

--- lib/teiserver/tachyon/config.ex
+++ lib/teiserver/tachyon/config.ex
@@ -2,6 +2,7 @@ defmodule Teiserver.Tachyon.Config do
   alias Teiserver.Config
 
   @restore_state_key "tachyon.should-restore-state"
+  @restore_timeout_key "tachyon.restore-timeout-ms"
 
   def setup_site_configs() do
     Config.add_site_config_type(%{
@@ -13,6 +14,15 @@ defmodule Teiserver.Tachyon.Config do
       default: false
     })
 
+    Config.add_site_config_type(%{
+      key: @restore_timeout_key,
+      section: "Tachyon",
+      type: "integer",
+      permissions: ["Admin"],
+      description: "How long a process should wait when restoring snapshot?",
+      default: 10_000
+    })
+
     :ok
   end
 
@@ -21,4 +31,13 @@ defmodule Teiserver.Tachyon.Config do
   def disable_state_restoration(), do: Config.update_site_config(@restore_state_key, false)
 
   def should_restore_state?(), do: Config.get_site_config_cache(@restore_state_key)
+
+  @spec get_restoration_timeout() :: non_neg_integer()
+  def get_restoration_timeout(), do: Config.get_site_config_cache(@restore_timeout_key)
+
+  @spec set_restoration_timeout(non_neg_integer()) :: :ok
+  def set_restoration_timeout(t), do: Config.update_site_config(@restore_timeout_key, t)
+
+  @spec reset_restoration_timeout() :: :ok
+  def reset_restoration_timeout(), do: Config.update_site_config(@restore_timeout_key, 10_000)
 end
