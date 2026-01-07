f8920b6776cc7ee59796a37adb0ba93749b85466
Tim Meißner <tim.meissner@haiilo.com>
2026-01-07 18:00:29 +0100

    Add default redirect uris to localhost for tachyon setup

--- lib/teiserver/tachyon/tasks/setup_apps.ex
+++ lib/teiserver/tachyon/tasks/setup_apps.ex
@@ -16,6 +16,11 @@ defmodule Teiserver.Tachyon.Tasks.SetupApps do
       uid: "generic_lobby",
       owner_id: root.id,
       scopes: ["tachyon.lobby"],
+      redirect_uris: [
+        "http://localhost/oauth2callback",
+        "http://127.0.0.1/oauth2callback",
+        "http://[::1]/oauth2callback"
+      ],
       description: "To support autohost and players using tachyon."
     })
   end
