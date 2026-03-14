e465c9e7d5bdbd798b54cb76590cb3f3e50542eb
David Norton <david.k.norton@gmail.com>
2026-03-14 15:14:45 -0500

    found another crashing page, fixed by passing the value through, but clearly we need the heex pages to fail softly rather than hard

---
+++ test/teiserver_web/views/battle_view_test.exs
@@ -0,0 +1,28 @@
+defmodule TeiserverWeb.BattleTest do
+  alias Teiserver.CacheUser
+  use TeiserverWeb.ConnCase, async: false
+  import Phoenix.LiveViewTest
+
+  alias Central.Helpers.GeneralTestLib
+  alias Teiserver.{TeiserverTestLib, Lobby}
+  import Teiserver.TeiserverTestLib, only: [_send_raw: 2, _recv_until: 1]
+  import Teiserver.Helper.NumberHelper, only: [int_parse: 1]
+
+  setup do
+    GeneralTestLib.conn_setup(Teiserver.TeiserverTestLib.player_permissions())
+    |> TeiserverTestLib.conn_setup()
+  end
+
+  describe "battle base path" do
+    test "index" do
+      {:ok, kw} =
+        GeneralTestLib.conn_setup()
+        |> Teiserver.TeiserverTestLib.conn_setup()
+
+      {:ok, conn} = Keyword.fetch(kw, :conn)
+
+      conn = get(conn, ~p"/battle")
+      html_response(conn, 200)
+    end
+  end
+end
