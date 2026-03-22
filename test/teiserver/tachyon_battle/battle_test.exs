350ef175965e4dbb7bf3c95a5eae5e80642935cb
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2026-03-22 09:00:20 +0000

    Refactor: test helper to create a battle+autohost
    These tests are more about specific battle operations, so it makes sense
    to abstract away the whole setup with the autohost, which is a bit
    verbose.

--- test/teiserver/tachyon_battle/battle_test.exs
+++ test/teiserver/tachyon_battle/battle_test.exs
@@ -10,63 +10,15 @@ defmodule Teiserver.TachyonBattle.BattleTest do
 
   describe "start battle" do
     test "happy path" do
-      autohost_id = :rand.uniform(10_000_000)
-      match_id = :rand.uniform(10_000_000)
+      %{battle_id: battle_id} = setup_autohost_and_battle()
 
-      {:ok, autohost_pid} = SessionSupervisor.start_session(%{id: autohost_id}, self())
-
-      receive do
-        {:call_client, "autohost/subscribeUpdates", _payload, ref} ->
-          send(ref, {ref, %{"status" => "success"}})
-      end
-
-      send(autohost_pid, {:update_capacity, 10, 0})
-
-      start_task =
-        Task.async(fn ->
-          {:ok, battle_id, _pid} =
-            Battle.start_battle_process(autohost_id, match_id, BotFixtures.start_script())
-
-          battle_id
-        end)
-
-      assert_receive {:start_battle, battle_id, _start_script}
-      resp = %{ips: ["1.2.3.4"], port: 12345}
-      Session.reply_start_battle(autohost_pid, battle_id, {:ok, resp})
-
-      battle_id = Task.await(start_task)
       poll_until_some(fn -> Battle.lookup(battle_id) end)
     end
   end
 
   describe "send message" do
     test "autohost is there" do
-      match_id = :rand.uniform(10_000_000)
-      autohost_id = :rand.uniform(10_000_000)
-
-      {:ok, autohost_pid} = SessionSupervisor.start_session(%{id: autohost_id}, self())
-
-      receive do
-        {:call_client, "autohost/subscribeUpdates", _payload, ref} ->
-          send(ref, {ref, %{"status" => "success"}})
-      end
-
-      send(autohost_pid, {:update_capacity, 10, 0})
-
-      start_task =
-        Task.async(fn ->
-          {:ok, battle_id, _pid} =
-            Battle.start_battle_process(autohost_id, match_id, BotFixtures.start_script())
-
-          battle_id
-        end)
-
-      assert_receive {:start_battle, battle_id, _start_script}
-      resp = %{ips: ["1.2.3.4"], port: 12345}
-      Session.reply_start_battle(autohost_pid, battle_id, {:ok, resp})
-
-      battle_id = Task.await(start_task)
-
+      %{battle_id: battle_id, autohost_pid: autohost_pid} = setup_autohost_and_battle()
       msg_task = Task.async(fn -> Battle.send_message(battle_id, "hello") end)
 
       assert_receive {:send_message, ref, %{battle_id: ^battle_id, message: "hello"}}
@@ -76,6 +28,16 @@ defmodule Teiserver.TachyonBattle.BattleTest do
   end
 
   test "kill battle" do
+    %{battle_id: battle_id, autohost_pid: autohost_pid} = setup_autohost_and_battle()
+
+    task = Task.async(fn -> Battle.kill(battle_id) end)
+    assert_receive {:kill_battle, ref, ^battle_id}
+    Session.reply_kill_battle(autohost_pid, ref, :ok)
+    assert Task.await(task) == :ok
+  end
+
+  # setup a handshaked autohost with some defaults
+  defp setup_autohost_and_battle() do
     match_id = :rand.uniform(10_000_000)
     autohost_id = :rand.uniform(10_000_000)
 
@@ -94,15 +56,17 @@ defmodule Teiserver.TachyonBattle.BattleTest do
           Battle.start_battle_process(autohost_id, match_id, BotFixtures.start_script())
       end)
 
-    assert_receive {:start_battle, battle_id, _start_script}
+    assert_receive {:start_battle, battle_id, start_script}
     resp = %{ips: ["1.2.3.4"], port: 12345}
     Session.reply_start_battle(autohost_pid, battle_id, {:ok, resp})
     Task.await(start_task)
 
-    task = Task.async(fn -> Battle.kill(battle_id) end)
-    assert_receive {:kill_battle, ref, ^battle_id}
-
-    Session.reply_kill_battle(autohost_pid, ref, :ok)
-    assert Task.await(task) == :ok
+    %{
+      match_id: match_id,
+      autohost_id: autohost_id,
+      autohost_pid: autohost_pid,
+      battle_id: battle_id,
+      start_script: start_script
+    }
   end
 end
