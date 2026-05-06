b8195cdea3ee04c363c4588bf4e44d7f68865029
Teifion <teifion@teifion.co.uk>
2026-05-06 11:13:52 +0100

    Upgraded version of plug_cowboy

--- mix.exs
+++ mix.exs
@@ -83,7 +83,7 @@ defmodule Teiserver.MixProject do
       {:peep, "~> 3.5.0"},
       {:gettext, "~> 0.20"},
       {:jason, "~> 1.2"},
-      {:plug_cowboy, "~> 2.5"},
+      {:plug_cowboy, "~> 2.8"},
 
       # Temporary deps while we transition away from views
       {:phoenix_view, "~> 2.0"},
