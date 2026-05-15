ca572f8c9702bf095d0d6a902db939297769b6cb
Lexon <44340857+L-e-x-o-n@users.noreply.github.com>
2026-05-15 09:11:06 +0200

    Remove match membership stats

--- lib/teiserver/mix_tasks/fake_data.ex
+++ lib/teiserver/mix_tasks/fake_data.ex
@@ -395,14 +395,6 @@ defmodule Mix.Tasks.Teiserver.Fakedata do
               %{
                 team_id: 0,
                 win: match.winning_team == 0,
-                stats: %{
-                  "damageDealt" => :rand.uniform(1000) * 10,
-                  "damageReceived" => :rand.uniform(1000) * 10,
-                  "metalProduced" => :rand.uniform(1000) * 100,
-                  "metalUsed" => :rand.uniform(1000) * 100,
-                  "energyProduced" => :rand.uniform(1000) * 1000,
-                  "energyUsed" => :rand.uniform(1000) * 1000
-                },
                 party_id: get_party_id(num_players),
                 user_id: userid,
                 match_id: match.id
@@ -415,14 +407,6 @@ defmodule Mix.Tasks.Teiserver.Fakedata do
               %{
                 team_id: 1,
                 win: match.winning_team == 1,
-                stats: %{
-                  "damageDealt" => :rand.uniform(1000) * 10,
-                  "damageReceived" => :rand.uniform(1000) * 10,
-                  "metalProduced" => :rand.uniform(1000) * 100,
-                  "metalUsed" => :rand.uniform(1000) * 100,
-                  "energyProduced" => :rand.uniform(1000) * 1000,
-                  "energyUsed" => :rand.uniform(1000) * 1000
-                },
                 party_id: get_party_id(num_players),
                 user_id: userid,
                 match_id: match.id
