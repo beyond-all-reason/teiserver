3d8cd5118be859c7ad61e630c0100a814ab43e1f
Teifion <teifion@teifion.co.uk>
2026-05-19 10:37:42 +0100

    Added test and cache reset on totp change

---
+++ test/teiserver/account/auth_test.exs
@@ -0,0 +1,34 @@
+defmodule Teiserver.Account.AuthTest do
+  alias Teiserver.Account
+  alias Teiserver.Account.AuthLib
+  alias Teiserver.AccountFixtures
+
+  use Teiserver.DataCase, async: false
+
+  setup %{} do
+    config = Application.get_env(:teiserver, Teiserver)
+    new_config = Keyword.put(config, :require_mfa_for_privileged_roles, true)
+    Application.put_env(:teiserver, Teiserver, new_config)
+
+    user = AccountFixtures.user_fixture(%{roles: ["Server"], permissions: ["Server"]})
+
+    on_exit(fn ->
+      Application.put_env(:teiserver, Teiserver, config)
+    end)
+
+    %{user: user}
+  end
+
+  describe "MFA functions" do
+    test "allow when no MFA", %{user: user} do
+      refute AuthLib.has_active_mfa?(user.id)
+      refute AuthLib.allow?(user, "Server")
+    end
+
+    test "allow when MFA present", %{user: user} do
+      Account.set_secret(user.id, "secret")
+      assert AuthLib.has_active_mfa?(user.id)
+      assert AuthLib.allow?(user, "Server")
+    end
+  end
+end
