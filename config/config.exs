b187a13bb3db7520859da964b9f9b878770fb3c6
Teifion <teifion@teifion.co.uk>
2026-06-16 10:15:18 +0100

    Fixed a bad assets export path for js.app (#1293)

--- config/config.exs
+++ config/config.exs
@@ -88,7 +88,7 @@ config :esbuild,
   version: "0.25.4",
   default: [
     args:
-      ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets/js --external:/fonts/* --external:/images/* --alias:@=.),
+      ~w(js/app.js --bundle --target=es2022 --outdir=../priv/static/assets --external:/fonts/* --external:/images/* --alias:@=.),
     cd: Path.expand("../assets", __DIR__),
     env: %{"NODE_PATH" => [Path.expand("../deps", __DIR__), Mix.Project.build_path()]}
   ]
