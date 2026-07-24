b5350956951ae303c130f1d307126aee1dd705ad
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2026-07-24 21:34:06 +0200

    Only log the route, not the actual path
    for now, this will avoid adding some garbage into the DB

--- lib/teiserver/logging/lib/logging_plug.ex
+++ lib/teiserver/logging/lib/logging_plug.ex
@@ -58,7 +58,10 @@ defmodule Teiserver.Logging.LoggingPlug do
   defp log_view(conn, start_tick, ip) do
     user_id = get_user_id(conn)
 
-    [_empty, section | path] = String.split(conn.request_path, "/")
+    route_info =
+      Phoenix.Router.route_info(TeiserverWeb.Router, conn.method, conn.request_path, conn.host)
+
+    [_empty, section | _rest] = String.split(conn.request_path, "/")
 
     # Log as seconds
     # load_time = (:os.system_time(:micro_seconds) - start_tick)/1000000
@@ -72,7 +75,9 @@ defmodule Teiserver.Logging.LoggingPlug do
     page_log =
       PageViewLog.changeset(%PageViewLog{}, %{
         section: section,
-        path: Enum.join(path, "/"),
+        # don't log the actual path with the params in them as this
+        # is going to generate a lot of noise and catch garbage
+        path: route_info.route,
         method: conn.method,
         ip: ip,
         load_time: load_time,
