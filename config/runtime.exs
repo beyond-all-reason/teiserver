6dbe75b0603f89e5d50291b2f544a0936a8bb877
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2026-07-26 10:14:58 +0200

    list allowed origins
    When we switched off TLS handling for teiserver in
    https://github.com/beyond-all-reason/ansible-teiserver/pull/29
    it broke websockets (liveviews).
    
    This is because the `check_origin: :conn` checks the `scheme`, and in this
    case the browser would send `https`, but the conn would have `http`.
    I didn't find a way to check host+port match between the origin header
    and the connection.
    The fix of listing all allowed domain is a bit meh since it's one more
    thing to support when moving/adding domains and we're going to forget
    that. But at the same time, it's also simple and shouldn't happen too
    often.

--- config/runtime.exs
+++ config/runtime.exs
@@ -68,7 +68,7 @@ config :teiserver, Teiserver.Repo,
 
 check_origin =
   if Teiserver.ConfigHelpers.get_env("TEI_SHOULD_CHECK_ORIGIN", false, :bool) do
-    :conn
+    ["//#{domain_name}"]
   else
     false
   end
