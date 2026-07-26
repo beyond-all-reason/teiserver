3e364b46c9fc310f79654aeacb0b55fa6634b051
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2026-07-26 13:07:10 +0200

    silence error about invalid origins
    It is incredibly verbose and annoying in the logs.

---
+++ lib/teiserver/logging/filter.ex
@@ -0,0 +1,19 @@
+defmodule Teiserver.Logging.Filter do
+  @moduledoc """
+  Some additional logging configuration
+  """
+
+  def filter(event, _args) do
+    if event.meta.mfa == {Phoenix.Socket.Transport, :check_origin, 5},
+      do: :stop,
+      else: :ignore
+  end
+
+  @doc """
+  add a filter to the loggers so that it doesn't show the very verbose message
+  about a connection's host that doesn't match the origin header.
+  """
+  def silence_bogus_origins do
+    :logger.add_primary_filter(:silence_bogus_origins, {&filter/2, []})
+  end
+end
