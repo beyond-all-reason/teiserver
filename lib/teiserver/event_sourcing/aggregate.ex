5a1b752a2509a992e6a33576f1198546057f3a97
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2026-08-02 07:52:05 +0200

    temp stuff not to break clients
    because this is a required field, if clients upgrade to the new schema,
    they will break. This is incorrect but at least it won't trigger a parse
    error.

---
+++ lib/teiserver/event_sourcing/aggregate.ex
@@ -0,0 +1,17 @@
+defmodule Teiserver.EventSourcing.Aggregate do
+  @moduledoc """
+  An aggregate is used when processing lobby events.
+  Reducing events from an initial aggregate leads to a new aggregate that holds
+  the new state, and maps of changes to be broadcasted to member or a new overview
+  for the lobby list.
+
+  Some events may also generate side effects, like an end vote. These are
+  represented in the aggregate as well.
+
+  Computing an aggregate must be a pure operation, that is, there must not be
+  any side effects. This is to guarantee we can replicate the state
+  in other processes without having to deal with duplicate messages.
+  In this context, events are data structure that represent changes to a lobby,
+  there is no message passing or IO involved.
+  """
+end
