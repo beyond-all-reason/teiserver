ed20ac529b388cbcb0a920067ed9b94920164a3f
Grégoire Charvet 黑瓜 <greg@geekingfrog.com>
2026-08-30 18:07:59 +0200

    refactor player password for matchmaking
    Instead of letting the player's session choose the password when
    pairing, leave that to whatever is constructing the start script. This
    is more consistent with how battle in lobbies is done, and allow us to
    keep track of less state in the session, which is always good.

--- lib/teiserver/player.ex
+++ lib/teiserver/player.ex
@@ -83,9 +83,9 @@ defmodule Teiserver.Player do
   @doc """
   It's go time! the player should join a game
   """
-  @spec battle_start(User.id(), {TachyonBattle.id(), pid()}, start_data()) ::
+  @spec battle_start(User.id(), {TachyonBattle.id(), pid()}, start_data(), password :: String.t()) ::
           :ok
-  defdelegate battle_start(user_id, battle_id, battle_start_data), to: Player.Session
+  defdelegate battle_start(user_id, battle_id, battle_start_data, password), to: Player.Session
 
   @doc """
   Let the player know that they can join a battle associated with a lobby
--- lib/teiserver/player/types/mm_pairing_state.ex
+++ lib/teiserver/player/types/mm_pairing_state.ex
@@ -7,16 +7,13 @@ defmodule Teiserver.Player.Types.MmPairingState do
   alias Teiserver.Matchmaking
 
   @enforce_keys [:paired_queue, :room, :frozen_queues, :readied?]
-  defstruct [:paired_queue, :room, :frozen_queues, :readied?, battle_password: ""]
-  # TODO: remove the battle password from there. It should only be a battle
-  # concern, and let the player know about it when joining.
+  defstruct [:paired_queue, :room, :frozen_queues, :readied?]
 
   @type t :: %__MODULE__{
           paired_queue: {Matchmaking.queue_id(), version :: String.t()},
           room: pid(),
           # a list of the other queues to rejoin in case the pairing fails
           frozen_queues: [{Matchmaking.queue_id(), version :: String.t()}],
-          readied?: boolean(),
-          battle_password: String.t()
+          readied?: boolean()
         }
 end
