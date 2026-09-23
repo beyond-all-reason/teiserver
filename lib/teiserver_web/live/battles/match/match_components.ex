e389af9b487ad2ece00399260269b7840259408a
Lexon <44340857+L-e-x-o-n@users.noreply.github.com>
2026-09-23 13:02:23 +0200

    Revert ratings page removal (#1612)

--- lib/teiserver_web/live/battles/match/match_components.ex
+++ lib/teiserver_web/live/battles/match/match_components.ex
@@ -23,6 +23,15 @@ defmodule TeiserverWeb.Battle.MatchComponents do
       List
     </.section_menu_button>
 
+    <.section_menu_button
+      bsname={@view_colour}
+      icon={Teiserver.Account.RatingLib.icon()}
+      active={@active == "ratings"}
+      url={~p"/battle/ratings"}
+    >
+      Ratings
+    </.section_menu_button>
+
     <%= if @match_id do %>
       <.section_menu_button
         bsname={@view_colour}
